df603c88dcfa001f86388b6f5f3474b8156381ec
rori <aurora@wm41b-664.cable.rcn.com>
2019-01-24 22:17:13 -0500

    add google login

---
+++ src/style.js
@@ -0,0 +1,42 @@
+import _ from 'underscore'
+import { style } from 'react-hyperscript-helpers'
+// import toCss from 'to-css'
+
+
+const colors = {
+  darkBluish: '#607d8b',
+  lightBluish: '#cfd8dc'
+}
+
+const elements = {
+  h2AndH3: { color: '#444', fontWeight: 'lighter' },
+  button: {
+    color: '#888', backgroundColor: '#eee',
+    border: 'none', borderRadius: 4,
+    padding: '5px 10px',
+    cursor: 'pointer'
+  }
+}
+
+/**
+ * Takes an element, props with a hoverStyle object, and children,
+ * and causes those styles to be applied on hover. Returns the new element.
+ ${toCss(props.hoverStyle, {
+   value: function(value) {
+     return value + ' !important'
+   }
+ })}
+ */
+const addHoverStyle = function(element, props, children) {
+  const hoverId = 'hover-' + Math.random()
+  const cleanedProps = _.omit(props, 'hoverStyle')
+  cleanedProps['data-hover-style-id'] = hoverId
+  const cssString =
+    `[data-hover-style-id="${hoverId}"]:hover {
+
+  }`
+
+  return element(cleanedProps, [children, style({}, cssString)])
+}
+
+export { colors, elements, addHoverStyle }
