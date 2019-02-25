0f2698d9590012377bb78664623b5c0630d5e5c7
rori <aurora@wm41b-664.broadinstitute.org>
2019-02-25 13:26:58 -0500

    add login / logout to welcome view

--- src/components/WelcomeView.jsx
+++ src/components/WelcomeView.jsx
@@ -1,35 +1,61 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { GoogleLogin } from 'react-google-login';
 
+import { logOut, logIn } from 'actions/index';
+
+
 class WelcomeView extends React.PureComponent {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired
+  };
+
+
   render() {
-    const responseGoogle = response => {
-      console.log(response);
+    const onSignInFailure = response => {
+      const { dispatch } = this.props;
+      dispatch(logOut());
+    };
+
+    const onSignInSuccess = response => {
+      const { dispatch } = this.props;
+      dispatch(logIn());
     };
     return (
       <div>
+        <h1>Welcome to the Jade Data Repository</h1>
         <div>
-          <GoogleLogin
-            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          <GoogleLogin // stealing clientId from Terra
+            clientId="500025638838-s2v23ar3spugtd5t2v1vgfa2sp7ppg0d.apps.googleusercontent.com"
             buttonText="Sign in with Google"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
+            onSuccess={onSignInSuccess}
+            onFailure={onSignInFailure}
             theme="dark"
           />
         </div>
 
         <h3>New User?</h3>
         <h3>Data Repository Requires a Terra Google account.</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Aenean euismod bibendum laoreet.
+          Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.
+          Proin sodales pulvinar sic tempor.
+        </p>
         <a ref="https://app.terra.bio/">Need to create a Terra account?</a>
         <hr />
 
         <div>
           <div>
-            Warning Notice Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
-            bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.
-            Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient
-            montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis
-            tellus mollis orci, sed rhoncus pronin sapien nunc accuan eget.
+            <div>Warning Notice</div>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
+              bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.
+              Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient
+              montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis
+              tellus mollis orci, sed rhoncus pronin sapien nunc accuan eget.
+            </div>
           </div>
         </div>
       </div>
