3ba8d4f3ba0ba14136edb98e1552001b76f554b5
rori <aurora@wm41b-664.broadinstitute.org>
2019-02-25 14:34:39 -0500

    add latin instead of hello word

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import WelcomeView from './WelcomeView';
+import config from 'config';
 
 class HomeView extends React.PureComponent {
   render() {
     return (
       <div>
-        Hello world home
-        <WelcomeView />
+        <h1>Welcome to the {config.description}</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.
+          Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales
+          pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci,
+          sed rhoncus pronin sapien nunc accuan eget.
+        </p>
+        <div>Put the collections component here</div>
       </div>
     );
   }
