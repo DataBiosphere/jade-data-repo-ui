5a670b20b4d0e757d35c128b3c3eb3bcfff77f07
rori <aurora@wm41b-664.broadinstitute.org>
2019-02-26 13:32:36 -0500

    swap out latin

--- src/components/WelcomeView.jsx
+++ src/components/WelcomeView.jsx
@@ -36,25 +36,26 @@ class WelcomeView extends React.PureComponent {
         </div>
 
         <h3>New User?</h3>
-        <h3>Data Repository Requires a Terra Google account.</h3>
+        <h3>Jade Data Repository requires a  Google account.</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Aenean euismod bibendum laoreet.
-          Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.
-          Proin sodales pulvinar sic tempor.
+          Terra uses your Google account. Once you have signed in and completed the user profile registration step, you can start using Terra.
         </p>
         <a ref="https://app.terra.bio/">Need to create a Terra account?</a>
         <hr />
 
         <div>
           <div>
-            <div>Warning Notice</div>
+            <div>WARNING NOTICE</div>
             <div>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod
-              bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.
-              Proin sodales pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient
-              montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis
-              tellus mollis orci, sed rhoncus pronin sapien nunc accuan eget.
+              You are accessing a US Government web site which may contain information that must be protected under the US Privacy Act or other sensitive information and is intended for Government authorized use only.
+
+              Unauthorized attempts to upload information, change information, or use of this web site may result in disciplinary action, civil, and/or criminal penalties. Unauthorized users of this website should have no expectation of privacy regarding any communications or data processed by this website.
+
+              Anyone accessing this website expressly consents to monitoring of their actions and all communications or data transiting or stored on related to this website and is advised that if such monitoring reveals possible evidence of criminal activity, NIH may provide that evidence to law enforcement officials.
+
+              WARNING NOTICE (when accessing TCGA controlled data)
+
+              You are reminded that when accessing TCGA controlled access information you are bound by the dbGaP TCGA DATA USE CERTIFICATION AGREEMENT (DUCA).
             </div>
           </div>
         </div>
