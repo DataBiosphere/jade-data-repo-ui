1ad6c084bfb8163778966af359f972899fb21801
rori <aurora@wm41b-664.broadinstitute.org>
2019-02-26 12:42:12 -0500

    remive unused login states

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -13,18 +13,10 @@ export default {
     {
       [ActionTypes.USER_LOGIN]: state =>
         immutable(state, {
-          status: { $set: STATUS.RUNNING },
-        }),
-      [ActionTypes.USER_LOGIN_SUCCESS]: state =>
-        immutable(state, {
           isAuthenticated: { $set: true },
           status: { $set: STATUS.READY },
         }),
       [ActionTypes.USER_LOGOUT]: state =>
-        immutable(state, {
-          status: { $set: STATUS.RUNNING },
-        }),
-      [ActionTypes.USER_LOGOUT_SUCCESS]: state =>
         immutable(state, {
           isAuthenticated: { $set: false },
           status: { $set: STATUS.IDLE },
