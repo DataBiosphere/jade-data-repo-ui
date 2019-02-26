a430d08b1b61a10c53ce3012150db36afe5c61b1
rori <aurora@wm41b-664.broadinstitute.org>
2019-02-26 12:43:27 -0500

    remove user sagas from index

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,12 +2,11 @@ import { all, fork } from 'redux-saga/effects';
 
 import app from './app';
 import github from './github';
-import user from './user';
-import repository from './resository';
+import repository from './repository';
 
 /**
  * rootSaga
  */
 export default function* root() {
-  yield all([fork(app), fork(github), fork(user), fork(repository)]);
+  yield all([fork(app), fork(github), fork(repository)]);
 }
