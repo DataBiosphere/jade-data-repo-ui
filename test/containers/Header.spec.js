88cb882aeb8c48ced872dea53b3f8f497d81157f
rori <aurora@wm41b-664.broadinstitute.org>
2019-02-27 11:58:53 -0500

    eslint styling

--- test/containers/Header.spec.js
+++ test/containers/Header.spec.js
@@ -1,21 +1,41 @@
 import React from 'react';
 
-import Header from 'containers/Header';
+import Header from 'components/Header';
 
 const mockDispatch = jest.fn();
 
-function setup() {
+function setup(isAuthenticated) {
   const props = {
+    app: {},
     dispatch: mockDispatch,
+    location: {
+      pathname: '/',
+    },
+    user: { isAuthenticated },
   };
 
-  return mount(<Header {...props} />);
+  return mount(<Header suppressClassNameWarning {...props} />);
 }
 
 describe('Header', () => {
-  const wrapper = setup();
+  let wrapper = setup(false);
 
   it('should render properly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  wrapper = setup(true);
+
+  it('should render properly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should handle clicks', () => {
+    wrapper.find('Logout').simulate('click');
+
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: 'USER_LOGOUT',
+      payload: {},
+    });
   });
 });
