2dbc4f329ffd0b20cedaa2ecf4e13a6cfc9dd6ca
rori <aurora@wm41b-664.broadinstitute.org>
2019-03-05 12:50:44 -0500

    small styling treatment

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import Icon from 'assets/media/images/white-jade.png';
 import { withStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-const styles = {
+const styles = theme => ({
   logoStyles: {
     alignItems: 'flex-start',
     display: 'inline-flex',
@@ -19,7 +19,7 @@ const styles = {
     left: '4px',
     position: 'relative',
   },
-};
+});
 
 class Logo extends React.PureComponent {
   static propTypes = {
@@ -35,6 +35,6 @@ class Logo extends React.PureComponent {
       </div>
     );
   }
-}
+};
 
 export default withStyles(styles)(Logo);
--- src/components/WelcomeView.jsx
+++ src/components/WelcomeView.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { GoogleLogin } from 'react-google-login';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 import Hero from 'assets/media/images/hero.png';
 import { logOut, logIn } from 'actions/index';
