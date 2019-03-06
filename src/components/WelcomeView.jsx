d2b6b5e4d713793857c5cab4432cbd1e5ae3ce15
rori <aurora@wm41b-664.broadinstitute.org>
2019-03-06 17:39:16 -0500

    styling pass on welcome page

--- src/components/WelcomeView.jsx
+++ src/components/WelcomeView.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { GoogleLogin } from 'react-google-login';
 import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 
+import Hero from 'assets/media/images/hero.png';
 import { logOut, logIn } from 'actions/index';
 
 const styles = theme => ({
   container: {
-    color: theme.typography.color,
-    flexGrow: 1,
-    padding: theme.spacing.unit * 10,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   title: {
     color: theme.palette.primary.main,
@@ -19,19 +19,39 @@ const styles = theme => ({
     paddingBottom: theme.spacing.unit * 8,
   },
   mainContent: {
-    justifyContent: 'center',
+    display: 'inline-block',
+    color: theme.typography.color,
+    overflow: 'hidden',
+    padding: theme.spacing.unit * 10,
+    width: '60%',
   },
   newUser: {
     color: theme.palette.secondary.contrastText,
     fontSize: '18px',
-    fontWeight: '200',
+    fontWeight: '300',
     paddingBottom: theme.spacing.unit * 2,
     paddingTop: theme.spacing.unit * 8
+  },
+  terraLink: {
+    color: theme.palette.primary.main,
+    paddingBottom: theme.spacing.unit * 4,
+    paddingTop: theme.spacing.unit * 2,
+    textDecoration: 'none',
   },
   header: {
     fontSize: '28px',
     lineHeight: '36px',
   },
+  heroContainer: {
+    display: 'inline-block',
+  },
+  hero: {
+    width: '500px',
+  },
+  warning: {
+    fontWeight: '900',
+    paddingBottom: theme.spacing.unit * 2,
+  }
 });
 
 class WelcomeView extends React.PureComponent {
@@ -54,47 +74,65 @@ class WelcomeView extends React.PureComponent {
 
     return (
       <div className={classes.container}>
-        <div className={classes.title}>
-          Welcome to the Jade Data Repository
-        </div>
         <div className={classes.mainContent}>
-          <GoogleLogin // stealing clientId from Terra
-            clientId="500025638838-s2v23ar3spugtd5t2v1vgfa2sp7ppg0d.apps.googleusercontent.com"
-            buttonText="Sign in with Google"
-            onSuccess={onSignInSuccess}
-            onFailure={onSignInFailure}
-            theme="dark"
-          />
-        </div>
-
-        <div className={classes.newUser}>New User?</div>
-        <div className={classes.header}>Jade Data Repository requires a Google account.</div>
-        <p>
-          Terra uses your Google account. Once you have signed in and completed the user profile
-          registration step, you can start using Terra.
-        </p>
-        <a href="https://app.terra.bio/">Need to create a Terra account?</a>
-        <hr />
-
-        <div>
+          <div className={classes.title}>
+          Welcome to the Jade Data Repository
+          </div>
           <div>
-            <div>WARNING NOTICE</div>
+            <GoogleLogin // stealing clientId from Terra
+              clientId="500025638838-s2v23ar3spugtd5t2v1vgfa2sp7ppg0d.apps.googleusercontent.com"
+              buttonText="Sign in with Google"
+              onSuccess={onSignInSuccess}
+              onFailure={onSignInFailure}
+              theme="dark"
+            />
+          </div>
+          <div className={classes.newUser}>New User?</div>
+          <div className={classes.header}>Jade Data Repository requires a Google account.</div>
+          <p>
+            Terra uses your Google account. Once you have signed in and completed the user profile
+            registration step, you can start using Terra.
+          </p>
+          <div href="https://app.terra.bio/" className={classes.terraLink} >Need to create a Terra account?</div>
+          <hr />
+          <div>
             <div>
-              You are accessing a US Government web site which may contain information that must be
-              protected under the US Privacy Act or other sensitive information and is intended for
-              Government authorized use only. Unauthorized attempts to upload information, change
-              information, or use of this web site may result in disciplinary action, civil, and/or
-              criminal penalties. Unauthorized users of this website should have no expectation of
-              privacy regarding any communications or data processed by this website. Anyone
-              accessing this website expressly consents to monitoring of their actions and all
-              communications or data transiting or stored on related to this website and is advised
-              that if such monitoring reveals possible evidence of criminal activity, NIH may
-              provide that evidence to law enforcement officials. WARNING NOTICE (when accessing
-              TCGA controlled data) You are reminded that when accessing TCGA controlled access
-              information you are bound by the dbGaP TCGA DATA USE CERTIFICATION AGREEMENT (DUCA).
+              <div className={classes.warning}>WARNING NOTICE</div>
+              <div>
+                <p>
+                  You are accessing a US Government web site which may contain information that must be
+                  protected under the US Privacy Act or other sensitive information and is intended for
+                  Government authorized use only.
+                </p>
+                <p>
+                  Unauthorized attempts to upload information, change information,
+                  or use of this web site may result in disciplinary action, civil, and/or
+                  criminal penalties. Unauthorized users of this website should have no expectation
+                  of privacy regarding any communications or data processed by this website.
+                </p>
+                <p>
+                  Anyone accessing this website expressly consents to monitoring of their actions
+                  and all communications or data transiting or stored on related to this website
+                  and is advised that if such monitoring reveals possible evidence of criminal
+                  activity, NIH may provide that evidence to law enforcement officials.
+                </p>
+                <p>
+                  WARNING NOTICE (when accessing TCGA controlled data)
+                </p>
+                <p>
+                  You are reminded that when accessing TCGA controlled accessinformation you are
+                  bound by the dbGaP TCGA DATA USE CERTIFICATION AGREEMENT (DUCA).
+                </p>
+              </div>
             </div>
           </div>
         </div>
+
+        <div className={classes.heroContainer}>
+          <img src={Hero} alt="hero" className={classes.hero} />
+        </div>
+
+
       </div>
     );
   }
