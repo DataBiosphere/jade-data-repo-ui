f5df1345e9649fe69a1af0c5a0224ab819c1e25e
rori <aurora@wm41b-664.broadinstitute.org>
2019-03-07 12:15:56 -0500

    studyview updates

--- src/components/StudyView.jsx
+++ src/components/StudyView.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
-class DatasetView extends React.PureComponent {
-  static propTypes = {};
+
+const styles = theme => ({
+  title: {
+    color: theme.palette.primary.main,
+    fontSize: '54px',
+    lineHeight: '66px',
+    paddingBottom: theme.spacing.unit * 8,
+  }
+});
+
+class StudyView extends React.PureComponent {  static propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
   render() {
-    return <div>Hello world studies</div>;
+    const { classes } = this.props;
+    return (
+      <div>
+        <div className={classes.title} >About Studies</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.
+          Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales
+          pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci,
+          sed rhoncus pronin sapien nunc accuan eget.
+        </p>
+        <div>
+          STUDIES
+        </div>
+      </div>
+    );
   }
 }
 
@@ -14,4 +41,5 @@ function mapStateToProps() {
   return {};
 }
 
-export default connect(mapStateToProps)(DatasetView);
+export default connect(mapStateToProps)(withStyles(styles)(StudyView));
+
