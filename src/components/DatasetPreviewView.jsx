63f6e5e24a4d2a8fd21f1ca2d3d6f5caa56a7bc6
rori <aurora@wm41b-664.broadinstitute.org>
2019-03-12 13:11:15 -0400

    niti preview

---
+++ src/components/DatasetPreviewView.jsx
@@ -0,0 +1,293 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import MultiSelect from 'react-select';
+import { Control, Form, actions, Errors } from 'react-redux-form';
+import _ from 'lodash';
+import xlsx from 'xlsx';
+import Combinatorics from 'js-combinatorics';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+import { createDataset } from 'actions/index';
+import ManageUsers from './ManageUsersView';
+
+function* colNameIter() {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 0; i < 3; i++) {
+    const args = [];
+    for (let j = 0; j <= i; j++) {
+      args.push(alphabet);
+    }
+    const product = Combinatorics.cartesianProduct.apply(null, args);
+    let item = product.next();
+    while (item) {
+      yield item.join('');
+      item = product.next();
+    }
+  }
+}
+
+function getColumnForField(sheet, assetField) {
+  // go through column headers A1, B1, C1, ..., see if they match the asset root column
+  for (const column of colNameIter(sheet)) {
+    if (sheet[column + 1]) {
+      if (assetField === sheet[column + 1].v) {
+        return column;
+      }
+    } else {
+      break;
+    }
+  }
+  return 'A';
+}
+
+const studyOptions = [
+  { value: 'Minimal', label: 'Minimal' },
+  { value: 'study_b', label: 'Study B' },
+  { value: 'study_c', label: 'Study C' },
+  { value: 'study_d', label: 'Study D' },
+  { value: 'study_e', label: 'Study E' },
+  { value: 'study_f', label: 'Study F' },
+  { value: 'study_g', label: 'Study G' },
+];
+
+const assetOptions = [
+  { value: 'participant', label: 'Participant' },
+  { value: 'sample', label: 'Sample' },
+];
+
+export class DatasetCreateView extends React.PureComponent {
+  static propTypes = {
+    asset: PropTypes.string,
+    dispatch: PropTypes.func.isRequired,
+    ids: PropTypes.arrayOf(PropTypes.string),
+    readers: PropTypes.arrayOf(PropTypes.string),
+    study: PropTypes.string,
+  };
+
+  handleSubmit(dataset) {
+    const { dispatch } = this.props;
+    const payload = {
+      name: dataset.name,
+      description: dataset.description,
+      contents: [
+        {
+          source: {
+            studyName: dataset.study,
+            assetName: dataset.asset,
+          },
+          rootValues: dataset.ids,
+        },
+      ],
+    };
+    dispatch(createDataset(payload));
+  }
+
+  validateName(name) {
+    return name && name.length > 0 && name.length < 64;
+  }
+
+  selectStudy(study) {
+    const { dispatch } = this.props;
+    dispatch(actions.change('dataset.study', study));
+  }
+
+  selectAsset(asset) {
+    const { dispatch } = this.props;
+    dispatch(actions.change('dataset.asset', asset));
+  }
+
+  addReader(newEmail) {
+    const { dispatch, readers } = this.props;
+    if (!_.includes(readers, newEmail)) {
+      dispatch(actions.change('dataset.readers', _.concat(readers, newEmail)));
+    }
+  }
+
+  removeReader(removeableEmail) {
+    const { dispatch, readers } = this.props;
+    const newReaders = _.clone(readers);
+    _.remove(newReaders, r => r === removeableEmail);
+    dispatch(actions.change('dataset.readers', newReaders));
+  }
+
+  parseFile = event => {
+    const { dispatch } = this.props;
+    const { files } = event.target;
+    const assetField = 'Epic';
+    if (files.length > 0) {
+      const reader = new FileReader();
+      reader.addEventListener('load', e => {
+        const data = e.target.result;
+        const workbook = xlsx.read(data, { type: 'binary' });
+        const values = [];
+        // assuming the upload has one spreadsheet
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const col = getColumnForField(sheet, assetField);
+        for (let i = 1; ; i++) {
+          const cell = sheet[col + i];
+          if (cell) {
+            values.push(cell.v);
+          } else {
+            break;
+          }
+        }
+        dispatch(actions.change('dataset.ids', values));
+      });
+      reader.readAsBinaryString(files[0]);
+    } else {
+      dispatch(actions.change('dataset.ids', []));
+    }
+  };
+
+  render() {
+    const FormRow = props => <div style={{ paddingBottom: '1em' }}>{props.children}</div>;
+    const { asset, ids, readers, study } = this.props;
+
+    return (
+      <div>
+        <h2>Create Dataset</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.
+          Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales
+          pulvinar sic tempor. Sociis natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci,
+          sed rhoncus pronin sapien nunc accuan eget.
+        </p>
+        <Form model="dataset" onSubmit={data => this.handleSubmit(data)}>
+          <FormRow>
+            <Control.text
+              model="dataset.name"
+              id="dataset.name"
+              required
+              validators={{ name: this.validateName }}
+              component={props => (
+                <TextField
+                  {...props}
+                  placeholder="Dataset Name"
+                  style={{ width: '300px' }}
+                  variant="outlined"
+                />
+              )}
+            />
+          </FormRow>
+
+          <FormRow>
+            <Control.textarea
+              model="dataset.description"
+              id="dataset.description"
+              required
+              component={props => (
+                <TextField
+                  {...props}
+                  style={{ width: '800px' }} // fullWidth
+                  multiline
+                  placeholder="Add Dataset Description"
+                  rows="8"
+                  rowsMax="100"
+                  variant="outlined"
+                />
+              )}
+            />
+          </FormRow>
+          <FormRow>
+            <Control.custom
+              id="dataset.readers"
+              model="dataset.readers"
+              component={props => (
+                <ManageUsers
+                  {...props}
+                  addReader={newEmail => this.addReader(newEmail)}
+                  defaultValue="Custodian Email Address"
+                  removeReader={removeableEmail => this.removeReader(removeableEmail)}
+                  readers={readers}
+                />
+              )}
+            />
+          </FormRow>
+          <FormRow>
+            <Control.select
+              id="dataset.study"
+              model="dataset.study"
+              size="5"
+              component={props => (
+                <MultiSelect
+                  {...props}
+                  onChange={e => this.selectStudy(e.value)}
+                  options={studyOptions}
+                  placeholder="Search Studies"
+                  value={studyOptions.filter(option => option.value === study)}
+                />
+              )}
+            />
+          </FormRow>
+
+          <FormRow>
+            <Control.select
+              id="dataset.asset"
+              model="dataset.asset"
+              size="5"
+              component={props => (
+                <MultiSelect
+                  {...props}
+                  onChange={e => this.selectAsset(e.value)}
+                  options={assetOptions}
+                  placeholder="Select Asset Type..."
+                  value={assetOptions.filter(option => option.value === asset)}
+                />
+              )}
+            />
+          </FormRow>
+
+          <FormRow>
+            <input
+              type="file"
+              id="dataset.upload"
+              onChange={this.parseFile}
+              style={{ display: 'none' }}
+            />
+            <label htmlFor="dataset.upload">
+              <Button variant="contained" component="span" color="primary">
+                Import Ids
+              </Button>
+            </label>
+          </FormRow>
+
+          {ids && (
+            <FormRow>
+              <Control.select multiple={true} disabled={true} id="dataset.ids" model="dataset.ids">
+                <option>Preview</option>
+                {ids && ids.map(id => <option key={id}>{id}</option>)}
+              </Control.select>
+            </FormRow>
+          )}
+
+          <Errors model="dataset" />
+
+          <FormRow>
+            <Button variant="contained" type="button">
+              Cancel
+            </Button>
+            <Button variant="contained" type="submit" color="primary">
+              Preview Data
+            </Button>
+          </FormRow>
+        </Form>
+      </div>
+    );
+  }
+}
+
+/* istanbul ignore next */
+function mapStateToProps(state) {
+  return {
+    asset: state.dataset.asset,
+    ids: state.dataset.ids,
+    readers: state.dataset.readers,
+    study: state.dataset.study,
+  };
+}
+
+export default connect(mapStateToProps)(DatasetCreateView);
