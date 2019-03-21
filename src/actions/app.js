28f04e6d7d7ea5ae47c850e44169bc7bf467dfc2
rori <aurora@wm41b-664.broadinstitute.org>
2019-03-21 15:54:58 -0400

    add dataset actions

--- src/actions/app.js
+++ src/actions/app.js
@@ -23,3 +23,8 @@ export const { getDatasets } = createActions({
   [ActionTypes.GET_DATASETS_SUCCESS]: datasets => datasets,
   [ActionTypes.GET_DATASETS]: datasets => datasets,
 });
+
+export const { getDatasetById } = createActions({
+  [ActionTypes.GET_DATASET_BY_ID]: dataset => dataset,
+  [ActionTypes.GET_DATASET_BY_ID_SUCCESS]: dataset => dataset,
+});
