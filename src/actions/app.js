13293110f5813ac2a484a9746673d29b5b191034
rori <aurora@wm41b-664.broadinstitute.org>
2019-03-22 15:39:36 -0400

    add study actions

--- src/actions/app.js
+++ src/actions/app.js
@@ -28,3 +28,8 @@ export const { getDatasetById } = createActions({
   [ActionTypes.GET_DATASET_BY_ID]: dataset => dataset,
   [ActionTypes.GET_DATASET_BY_ID_SUCCESS]: dataset => dataset,
 });
+
+export const { getStudyById } = createActions({
+  [ActionTypes.GET_STUDY_BY_ID]: study => study,
+  [ActionTypes.GET_STUDY_BY_ID_SUCCESS]: study => study,
+});
