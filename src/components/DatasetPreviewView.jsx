473d719c015719f627e6fe90e65b1924c233293a
rori <aurora@wm41b-664.broadinstitute.org>
2019-04-07 13:05:56 -0400

    add directional modal dataset creation success

--- src/components/DatasetPreviewView.jsx
+++ src/components/DatasetPreviewView.jsx
@@ -5,6 +5,12 @@ import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 import { createDataset } from 'actions/index';
 
@@ -39,7 +45,7 @@ const styles = theme => ({
     padding: theme.spacing.unit * 4,
   },
   header: {
-    fontSize: '14px',
+    fontSize: theme.spacing.unit * 2,
     lineHeight: '22px',
     fontWeight: '600',
   },
@@ -51,9 +57,57 @@ const styles = theme => ({
     paddingBottom: theme.spacing.unit * 3,
     paddingTop: theme.spacing.unit * 8,
   },
+  dialogTitle: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: 0,
+    padding: theme.spacing.unit * 3,
+  },
+  dialogContent: {
+    paddingBottom: theme.spacing.unit * 4,
+    paddingTop: theme.spacing.unit * 4,
+    fontFamily: [
+      'Montserrat',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    fontSize: '20px',
+    fontWeight: '500',
+  },
+  dialogActions: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingBottom: theme.spacing.unit * 2,
+    paddingTop: theme.spacing.unit * 2,
+    width: '600px',
+  },
+  actionButtons: {
+    textDecoration: 'none',
+    color: theme.palette.primary.contrastText,
+  },
+  closetitle: {
+    display: 'inline-block',
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing.unit * 1.5,
+    top: theme.spacing.unit * 2,
+  },
 });
 
 export class DatasetPreviewView extends React.PureComponent {
+  state = {
+    open: false,
+  };
+
   static propTypes = {
     classes: PropTypes.object.isRequired,
     createdDataset: PropTypes.object,
@@ -79,11 +133,13 @@ export class DatasetPreviewView extends React.PureComponent {
     dispatch(createDataset(payload));
   }
 
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
     const { classes, createdDataset, dataset } = this.props;
-    // TODO when the job is completed -- what happens?
-    // what happens if you got to this page and we havent loaded data properly?
-    // what if it fails vs succeeds
+    const { open } = this.state;
     return (
       <div id="dataset-preview" className={classes.wrapper}>
         <div className={classes.width}>
@@ -91,6 +147,50 @@ export class DatasetPreviewView extends React.PureComponent {
             <div>
               <div className={classes.title}>Created Dataset</div>
               <p>Your new dataset has been created!</p>
+
+              <Dialog
+                onClose={this.handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
+                maxWidth={false}
+              >
+                <DialogTitle className={classes.dialogTitle} id="customized-dialog-title">
+                  <div className={classes.closetitle}>Where to Next?</div>
+                  <div>
+                    <IconButton
+                      aria-label="Close"
+                      onClick={this.handleClose}
+                      className={classes.closeButton}
+                    >
+                      <CloseIcon />
+                    </IconButton>
+                  </div>
+                </DialogTitle>
+                <DialogContent>
+                  <div className={classes.dialogContent}>
+                    The new dataset {createdDataset.name} has been created. What would you like to
+                    do next?
+                  </div>
+                  <div className={classes.dialogActions}>
+                    <Button color="primary" variant="contained">
+                      <Link to={`/datasets/${createdDataset.id}`} className={classes.actionButtons}>
+                        View new dataset
+                      </Link>
+                    </Button>
+                    <Button color="primary" variant="contained">
+                      <Link to="/datasets" className={classes.actionButtons}>
+                        View all datasets
+                      </Link>
+                    </Button>
+                    <Button color="primary" variant="contained">
+                      <Link to="/datasets/create" className={classes.actionButtons}>
+                        Create another dataset
+                      </Link>
+                    </Button>
+                  </div>
+                </DialogContent>
+              </Dialog>
+
               <div className={classes.query}>
                 <LinearProgress variant="determinate" value={100} />
               </div>
@@ -109,7 +209,7 @@ export class DatasetPreviewView extends React.PureComponent {
               <div className={classes.header}> Dataset Name: </div>
               {createdDataset.id && createdDataset.name === dataset.name ? (
                 <div className={classes.values}>
-                  <Link to={`/dataset/${createdDataset.id}`}>{dataset.name}</Link>
+                  <Link to={`/datasets/${createdDataset.id}`}>{dataset.name}</Link>
                 </div>
               ) : (
                 <div className={classes.values}>{dataset.name}</div>
