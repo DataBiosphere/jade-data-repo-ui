e991c30b53df2de9dadde43a2adec243f60ec40e
rori <aurora@wm41b-664.broadinstitute.org>
2019-04-09 18:04:17 -0400

    add navlink back after merge conflict

--- src/components/table/DatasetTable.jsx
+++ src/components/table/DatasetTable.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
