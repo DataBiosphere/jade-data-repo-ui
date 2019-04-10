a55cae997544f315358a95d5bb746572b6eac471
Jeremy Hert <jhert@broadinstitute.org>
2019-04-10 11:18:40 -0400

    pass at using browserrouter

--- src/routes/Private.jsx
+++ src/routes/Private.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { HashRouter, Link, Switch, Route } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { withStyles } from '@material-ui/core/styles';
@@ -29,74 +29,63 @@ const styles = theme => ({
 });
 
 class Private extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-    };
-  }
-
   static propTypes = {
     classes: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
   };
 
-  componentDidUpdate(prevProps) {
-    const { location } = this.props;
-    if (location !== prevProps.location) {
-      this.onRouteChanged(location.hash.split('/')[1]);
-    }
-  }
-
-  onRouteChanged = value => {
-    this.setState({ value });
-  };
+  static prefixMatcher = new RegExp('/[^/]*');
 
   render() {
-    const { value } = this.state;
     const { classes } = this.props;
     return (
-      <HashRouter>
+      <Router>
         <div>
-          <Tabs
-            value={value}
-            classes={{ root: classes.tabsRoot, indicator: classes.tabsIndicator }}
-          >
-            <Tab
-              label="Home"
-              component={Link}
-              value=""
-              to="/"
-              classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
-            />
-            <Tab
-              label="Studies"
-              component={Link}
-              value="studies"
-              to="/studies"
-              classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
-            />
-            <Tab
-              label="Datasets"
-              component={Link}
-              value="datasets"
-              to="/datasets"
-              classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
-            />
-          </Tabs>
-          <Switch>
-            <div>
-              <Route exact path="/" component={HomeView} />
-              <Route exact path="/studies" component={StudiesView} />
-              <Route exact path="/studies/details/:uuid" component={StudyDetailView} />
-              <Route exact path="/datasets" component={DatasetView} />
-              <Route exact path="/datasets/create" component={DatasetCreateView} />
-              <Route exact path="/datasets/preview" component={DatasetPreviewView} />
-              <Route exact path="/datasets/details/:uuid" component={DatasetDetailView} />
-            </div>
-          </Switch>
+          <Route
+            path="/"
+            render={({ location }) => (
+              <Fragment>
+                <Tabs
+                  value={Private.prefixMatcher.exec(location.pathname)[0]}
+                  classes={{ root: classes.tabsRoot, indicator: classes.tabsIndicator }}
+                >
+                  <Tab
+                    label="Home"
+                    component={Link}
+                    value="/"
+                    to="/"
+                    classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+                  />
+                  <Tab
+                    label="Studies"
+                    component={Link}
+                    value="/studies"
+                    to="/studies"
+                    classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+                  />
+                  <Tab
+                    label="Datasets"
+                    component={Link}
+                    value="/datasets"
+                    to="/datasets"
+                    classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+                  />
+                </Tabs>
+                <Switch>
+                  <div>
+                    <Route exact path="/" component={HomeView} />
+                    <Route exact path="/studies" component={StudiesView} />
+                    <Route exact path="/studies/details/:uuid" component={StudyDetailView} />
+                    <Route exact path="/datasets" component={DatasetView} />
+                    <Route exact path="/datasets/create" component={DatasetCreateView} />
+                    <Route exact path="/datasets/preview" component={DatasetPreviewView} />
+                    <Route exact path="/datasets/details/:uuid" component={DatasetDetailView} />
+                  </div>
+                </Switch>
+              </Fragment>
+            )}
+          />
         </div>
-      </HashRouter>
+      </Router>
     );
   }
 }
