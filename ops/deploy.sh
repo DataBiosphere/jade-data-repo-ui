28889d05f08803a6e7fba8dffb0d88a8899935ce
Jeremy Hert <jhert@broadinstitute.org>
2019-04-11 09:29:20 -0400

    getting k8s working in integration

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# make sure that a project is set, similar to the api we will tag with this
+: ${GOOGLE_CLOUD_PROJECT:?}
+
 # the paths we'll use will be relative to this script
 WD=$( dirname "${BASH_SOURCE[0]}" )
 
@@ -9,9 +12,12 @@ WD=$( dirname "${BASH_SOURCE[0]}" )
 npm run build --production
 
 # buld the docker image
-docker build -t data-repo-ui ${WD}/..
+docker build -t jade-data-repo-ui:latest${GOOGLE_CLOUD_PROJECT} ${WD}/..
+docker push gcr.io/broad-jade-dev/jade-data-repo-ui:latest${GOOGLE_CLOUD_PROJECT}
 
 # delete the ui pod if it exists and recreate it
-kubectl --namespace data-repo get pod data-repo-ui && kubectl --namespace data-repo delete pod data-repo-ui
-kubectl apply -f "${WD}/k8s/ui-pod.yaml"
+kubectl apply -f "${WD}/k8s/ui-deployment.yaml"
 kubectl apply -f "${WD}/k8s/ui-service.yaml"
+
+kubectl --namespace data-repo set image deployments/ui-deployment \
+    "data-repo-ui-container=gcr.io/broad-jade-dev/jade-data-repo-ui:latest${GOOGLE_CLOUD_PROJECT}"
