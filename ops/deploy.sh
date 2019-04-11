2c532aa972324b4ecf38a5dafe6535652907a6d0
Jeremy Hert <jhert@broadinstitute.org>
2019-04-11 09:29:20 -0400

    getting k8s working in integration

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -8,13 +8,10 @@ WD=$( dirname "${BASH_SOURCE[0]}" )
 # build for production
 npm run build --production
 
-# switch into minikube mode (we want the containers we build to be available locally to minikube)
-eval $( minikube docker-env )
-
 # buld the docker image
 docker build -t data-repo-ui ${WD}/..
 
 # delete the ui pod if it exists and recreate it
 kubectl --namespace data-repo get pod data-repo-ui && kubectl --namespace data-repo delete pod data-repo-ui
-kubectl apply -f "${WD}/ui-pod.yaml"
-kubectl apply -f "${WD}/ui-service.yaml"
+kubectl apply -f "${WD}/k8s/ui-pod.yaml"
+kubectl apply -f "${WD}/k8s/ui-service.yaml"
