5c5bf1e82e05fe81c1ebca66a172d9ba1e6ea6d0
thathert <jeremy.hert@gmail.com>
2019-04-12 15:32:24 -0500

    getting k8s working in integration (#34)
    * getting k8s working in integration

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -2,19 +2,22 @@
 
 set -e
 
+# make sure that a project is set, similar to the api we will tag with this
+: ${GOOGLE_CLOUD_PROJECT:?}
+
 # the paths we'll use will be relative to this script
 WD=$( dirname "${BASH_SOURCE[0]}" )
 
 # build for production
 npm run build --production
 
-# switch into minikube mode (we want the containers we build to be available locally to minikube)
-eval $( minikube docker-env )
-
 # buld the docker image
-docker build -t data-repo-ui ${WD}/..
+TAG="gcr.io/broad-jade-dev/jade-data-repo-ui:latest${GOOGLE_CLOUD_PROJECT}"
+docker build -t $TAG ${WD}/..
+docker push $TAG
 
 # delete the ui pod if it exists and recreate it
-kubectl --namespace data-repo get pod data-repo-ui && kubectl --namespace data-repo delete pod data-repo-ui
-kubectl apply -f "${WD}/ui-pod.yaml"
-kubectl apply -f "${WD}/ui-service.yaml"
+kubectl apply -f "${WD}/k8s/ui-deployment.yaml"
+kubectl apply -f "${WD}/k8s/ui-service.yaml"
+
+kubectl --namespace data-repo set image deployments/ui-deployment "data-repo-ui-container=${TAG}"
