8f13190380288be2fdcf58424c65d70baa8d4dbf
rori <aurora@wm41b-664.cable.rcn.com>
2019-04-20 18:05:35 -0400

    fixup on toast

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
