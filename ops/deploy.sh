f70d00c3412f0553cd6b2e0ad32f2a7184d5e715
thathert <jeremy.hert@gmail.com>
2019-04-21 07:46:43 -0500

    new k8s (#40)
    * updates for the new k8s clusters
    
    * fix reverse proxy

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -6,18 +6,18 @@ set -e
 : ${GOOGLE_CLOUD_PROJECT:?}
 
 # the paths we'll use will be relative to this script
-WD=$( dirname "${BASH_SOURCE[0]}" )
+WD=$(dirname "${BASH_SOURCE[0]}")
+NOW=$(date +%Y-%m-%d_%H-%M-%S)
 
 # build for production
 npm run build --production
 
+kubectl apply -f "${WD}/k8s/ui-service.yaml"
+kubectl apply -f "${WD}/k8s/ui-deployment.yaml"
+
 # buld the docker image
-TAG="gcr.io/broad-jade-dev/jade-data-repo-ui:latest${GOOGLE_CLOUD_PROJECT}"
+TAG="gcr.io/broad-jade-dev/jade-data-repo-ui:${GOOGLE_CLOUD_PROJECT}_${NOW}"
 docker build -t $TAG ${WD}/..
 docker push $TAG
-
-# delete the ui pod if it exists and recreate it
-kubectl apply -f "${WD}/k8s/ui-deployment.yaml"
-kubectl apply -f "${WD}/k8s/ui-service.yaml"
 
 kubectl --namespace data-repo set image deployments/ui-deployment "data-repo-ui-container=${TAG}"
