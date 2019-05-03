5c9c8bfd872b6a6c01e743b853bd56757530a15a
rori <aurora@wm41b-664.cable.rcn.com>
2019-05-02 22:24:37 -0400

    add job reducer and constant

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import app from './app';
 import user from './user';
 import dataset from './dataset';
 import study from './study';
+import job from './job';
 
 export default {
   ...app,
   ...user,
   ...dataset,
   ...study,
+  ...job,
 };
