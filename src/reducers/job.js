cf4c57058b73a45ec66717bc81c4652d4ca12556
rori <aurora@wm41b-664.broadinstitute.org>
2019-05-19 22:03:51 -0400

    linting warnings

--- src/reducers/job.js
+++ src/reducers/job.js
@@ -13,31 +13,26 @@ export const jobState = {
 export default {
   jobs: handleActions(
     {
-      [ActionTypes.GET_JOB_BY_ID_SUCCESS]: (state, action) => {
-        return immutable(state, {
+      [ActionTypes.GET_JOB_BY_ID_SUCCESS]: (state, action) =>
+        immutable(state, {
           jobStatus: { $set: action.payload.status },
-        });
-      },
-      [ActionTypes.GET_JOB_RESULT_SUCCESS]: (state, action) => {
-        return immutable(state, {
+        }),
+      [ActionTypes.GET_JOB_RESULT_SUCCESS]: (state, action) =>
+        immutable(state, {
           jobResultObjectId: { $set: action.payload.data.id },
-        });
-      },
-      [ActionTypes.CREATE_DATASET_JOB]: (state, action) => {
-        return immutable(state, {
+        }),
+      [ActionTypes.CREATE_DATASET_JOB]: (state, action) =>
+        immutable(state, {
           jobId: { $set: action.payload.jobId },
-        });
-      },
-      [ActionTypes.CREATE_DATASET_SUCCESS]: state => {
-        return immutable(state, {
+        }),
+      [ActionTypes.CREATE_DATASET_SUCCESS]: state =>
+        immutable(state, {
           jobStatus: { $set: STATUS.SUCCESS },
-        });
-      },
-      [ActionTypes.CLEAR_JOB_ID]: state => {
-        return immutable(state, {
+        }),
+      [ActionTypes.CLEAR_JOB_ID]: state =>
+        immutable(state, {
           jobId: { $set: '' },
-        });
-      },
+        }),
     },
     jobState,
   ),
