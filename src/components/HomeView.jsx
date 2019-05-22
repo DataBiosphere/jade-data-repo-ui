fa10a485088b94dc3c1db8cc524366c9d23f87ec
rori <aurora@wm41b-664.broadinstitute.org>
2019-05-22 12:21:21 -0400

    remove studies get

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 
-import { getStudies } from 'actions/index';
 import StudyTable from './table/StudyTable';
 import DatasetTable from './table/DatasetTable';
 
@@ -32,34 +30,22 @@ const styles = theme => ({
 class HomeView extends React.PureComponent {
   static propTypes = {
     classes: PropTypes.object.isRequired,
-    dispatch: PropTypes.func.isRequired,
     studies: PropTypes.object,
   };
 
-  componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch(getStudies());
-  }
-
   render() {
-    const { classes, studies } = this.props;
+    const { classes } = this.props;
     return (
       <div className={classes.wrapper}>
         <div className={classes.width}>
           <div className={classes.title}>Terra Data Repository at a glance</div>
-          <div> {studies && studies.studies && <StudyTable rows={studies.studies} />} </div>
+          <StudyTable summary />
           <div className={classes.jadeTableSpacer} />
-          <DatasetTable />
+          <DatasetTable summary />
         </div>
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    studies: state.studies,
-  };
-}
-
-export default connect(mapStateToProps)(withStyles(styles)(HomeView));
+export default withStyles(styles)(HomeView);
