35e4de6603ec9109119d7bbd30ee24a67ae2c1dd
rori <aurora@wm41b-664.broadinstitute.org>
2019-05-23 16:13:20 -0400

    add see all datasets and studies to home page

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
-import StudyTable from './table/StudyTable';
+import { getDatasets, getStudies } from 'actions/index';
 import DatasetTable from './table/DatasetTable';
+import StudyTable from './table/StudyTable';
 
 const styles = theme => ({
   wrapper: {
@@ -25,6 +27,20 @@ const styles = theme => ({
   jadeTableSpacer: {
     paddingBottom: theme.spacing.unit * 8,
   },
+  jadeLink: {
+    color: theme.palette.primary.main,
+    float: 'right',
+    fontSize: 16,
+    fontWeight: 500,
+    height: 20,
+    letterSpacing: 0.3,
+    paddingLeft: theme.spacing.unit * 4,
+    paddingTop: theme.spacing.unit * 4,
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
 });
 
 class HomeView extends React.PureComponent {
@@ -40,8 +56,18 @@ class HomeView extends React.PureComponent {
         <div className={classes.width}>
           <div className={classes.title}>Terra Data Repository at a glance</div>
           <StudyTable summary />
+          <div>
+            <Link to="/studies" className={classes.jadeLink}>
+              See all Studies
+            </Link>
+          </div>
           <div className={classes.jadeTableSpacer} />
           <DatasetTable summary />
+          <div>
+            <Link to="/datasets" className={classes.jadeLink}>
+              See all Datasets
+            </Link>
+          </div>
         </div>
       </div>
     );
