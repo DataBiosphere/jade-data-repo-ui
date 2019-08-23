150608c890436ca55914ebf424ed2efbddb1856f
Michael Yessaillian <myessail@broadinstitute.org>
2019-08-23 16:56:31 -0400

    styling and stuff

--- src/components/dataset/query/QueryView.jsx
+++ src/components/dataset/query/QueryView.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
 
 import { runQuery } from 'actions/index';
 
