12518d8d71404b534d87864fb41cd0f877a53175
Michael Yessaillian <myessail@broadinstitute.org>
2019-11-04 15:53:35 -0500

    clear out freetext filter when the 'x' is pressed

--- src/components/dataset/query/sidebar/filter/FreetextFilter.jsx
+++ src/components/dataset/query/sidebar/filter/FreetextFilter.jsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import BigQuery from 'modules/bigquery';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 import Input from '@material-ui/core/Input';
 
 export class FreetextFilter extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      prevPropsFilterData: props.filterData,
+      currValue: '',
+    };
+  }
+
   static propTypes = {
     column: PropTypes.object,
+    filterData: PropTypes.object,
     handleChange: PropTypes.func,
   };
 
+  static getDerivedStateFromProps(props, state) {
+    if (!_.isEqual(props.filterData, state.prevPropsFilterData)) {
+      return {
+        currValue: _.get(props.filterData, `${props.column.name}`, ''),
+        prevPropsFilterData: props.filterData,
+      };
+    }
+    return null;
+  }
+
+  handleChange = event => {
+    const { handleChange } = this.props;
+    this.setState({
+      currValue: event.target.value,
+    });
+    handleChange(event);
+  };
+
   render() {
-    const { column, handleChange } = this.props;
+    const { column } = this.props;
+    const { currValue } = this.state;
     return (
       <Input
         placeholder={column.name}
-        onChange={handleChange}
+        onChange={this.handleChange}
         inputProps={{
           'aria-label': 'description',
         }}
+        value={currValue}
       />
     );
   }
