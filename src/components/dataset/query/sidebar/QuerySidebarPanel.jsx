11de6dfb92659b52a9011ba1d21ac391ffb61b9f
MacKenzie Kerwin <mackenziekerwin@gmail.com>
2020-01-16 11:53:05 -0500

    fixed bug in clearing range filters

--- src/components/dataset/query/sidebar/QuerySidebarPanel.jsx
+++ src/components/dataset/query/sidebar/QuerySidebarPanel.jsx
@@ -7,6 +7,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -19,6 +20,9 @@ const styles = () => ({
     paddingBottom: '4px',
     lineHeight: 'inherit',
   },
+  rangeInfo: {
+    display: 'inline',
+  }
 });
 
 export class QuerySidebarPanel extends React.PureComponent {
@@ -32,34 +36,41 @@ export class QuerySidebarPanel extends React.PureComponent {
     const { dispatch, filterData } = this.props;
     const clonedData = _.cloneDeep(filterData);
     const clonedFilter = clonedData[filter];
-    if (Array.isArray(clonedFilter)) {
-      clonedFilter.splice(clonedFilter.indexOf(datum), 1);
-    }
-    if (_.isPlainObject(clonedFilter)) {
-      delete clonedFilter[datum];
+    const filterValue = clonedFilter.value;
+    if (clonedFilter.type === 'range') {
+      delete clonedData[filter];
+    } else {
+      if (Array.isArray(filterValue)) {
+        filterValue.splice(filterValue.indexOf(datum), 1);
+      }
+      if (_.isPlainObject(filterValue)) {
+        delete filterValue[datum];
+      }
     }
     dispatch(applyFilters(clonedData));
   };
+
   render() {
     const { classes, filterData } = this.props;
     const listFilters = _.keys(filterData).map(filter => {
       const data = _.get(filterData, filter);
-      let dataString = data;
-      if (Array.isArray(data)) {
-        dataString = data.map(datum => {
+      let dataString = data.value;
+      if (data.type === 'range') {
+        dataString = (
+        <span>
+          <Typography className={classes.rangeInfo}>{_.join(data.value, ' \u2013 ')}</Typography>
+          <Button className={classes.rangeInfo} onClick={() => this.clearFilter(filter)}>
+            <HighlightOff />
+          </Button>
+        </span>
+        );
+      } else {
+        if (_.isPlainObject(data.value)) {
+          dataString = _.keys(data.value);
+        }
+        dataString = dataString.map((datum, i) => {
           return (
-            <ListItemText>
-              {datum}
-              <Button onClick={() => this.clearFilter(filter, datum)}>
-                <HighlightOff />
-              </Button>
-            </ListItemText>);
-        });
-      }
-      if (_.isPlainObject(data)) {
-        dataString = _.keys(data).map(datum => {
-          return (
-            <ListItemText>
+            <ListItemText key={i}>
               {datum}
               <Button onClick={() => this.clearFilter(filter, datum)}>
                 <HighlightOff />
