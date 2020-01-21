ed1e3a79c3ec1d48e5085acd058f4cc2bba54d4e
Michael Yessaillian <myessail@broadinstitute.org>
2020-01-21 11:20:34 -0500

    handle array values in the table (#150)

--- src/components/dataset/query/QueryView.jsx
+++ src/components/dataset/query/QueryView.jsx
@@ -6,7 +6,6 @@ import Grid from '@material-ui/core/Grid';
 
 import { applyFilters, runQuery, getDatasetById } from 'actions/index';
 import { Typography } from '@material-ui/core';
-import { DB_COLUMNS } from '../../../constants/index';
 
 import QueryViewSidebar from './sidebar/QueryViewSidebar';
 import QueryViewDropdown from './QueryViewDropdown';
