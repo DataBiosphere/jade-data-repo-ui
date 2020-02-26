2b5b5934bb73ee4732de2009d789d9b7c7a8ab6a
Michael Yessaillian <myessail@broadinstitute.org>
2020-02-26 13:59:55 -0500

    missing (another) lodash import

--- src/components/dataset/query/sidebar/filter/FreetextFilter.jsx
+++ src/components/dataset/query/sidebar/filter/FreetextFilter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 import TextField from '@material-ui/core/TextField';
 import Chip from '@material-ui/core/Chip';
