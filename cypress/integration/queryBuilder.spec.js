2304534559c967930b37e1c606e158c851fde5b0
Michael Yessaillian <myessail@broadinstitute.org>
2020-03-16 22:34:19 -0400

    add headless login

--- cypress/integration/queryBuilder.spec.js
+++ cypress/integration/queryBuilder.spec.js
@@ -1,6 +1,9 @@
 describe('test query builder functionality', () => {
   it('basic test', () => {
     cy.visit('/login/e2e');
+    cy.get('#tokenInput').type(Cypress.env('GOOGLE_TOKEN'), { log: false, delay: 0 });
+    cy.get('#e2eLoginButton').click();
+    cy.contains('V2F_GWAS_Summary_Stats').should('be.visible');
     cy.contains('V2F_GWAS_Summary_Stats').click();
     cy.get('p')
       .contains('V2F_GWAS_Summary_Stats')
--- src/routes/HeadlessLogin.jsx
+++ src/routes/HeadlessLogin.jsx
@@ -2,30 +2,47 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import { logIn } from '../actions/index';
 
 export class HeadlessLogin extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      token: '',
+    };
+  }
+
   static propTypes = {
     classes: PropTypes.object.isRequired,
     dispatch: PropTypes.func.isRequired,
   };
 
-  componentDidMount() {
-    const { dispatch } = this.props;
+  handleChange = event => {
+    this.setState({
+      token: event.target.value,
+    });
+  };
 
-    dispatch(
-      logIn(
-        'user.name',
-        'user.imageUrl',
-        'user.email',
-        process.env.REACT_APP_GOOGLE_TOKEN,
-        1615064728000,
-      ),
-    );
-  }
+  handleLoginButton = () => {
+    const { dispatch } = this.props;
+    const { token } = this.state;
+
+    dispatch(logIn('user.name', 'user.imageUrl', 'user.email', token, 1615064728000));
+  };
 
   render() {
-    return <div />;
+    return (
+      <div>
+        <h1>michael's secret login page</h1>
+        <TextField id="tokenInput" label="Password" type="password" onChange={this.handleChange} />
+        <Button id="e2eLoginButton" onClick={this.handleLoginButton}>
+          Log In
+        </Button>
+      </div>
+    );
   }
 }
 
