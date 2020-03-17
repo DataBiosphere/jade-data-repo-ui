9b75a5acf5d7ab3b4a38191cdaee3bdee77d55ae
Michael Yessaillian <myessail@broadinstitute.org>
2020-03-16 22:48:53 -0400

    increase selector timeout

--- cypress/integration/queryBuilder.spec.js
+++ cypress/integration/queryBuilder.spec.js
@@ -1,7 +1,10 @@
 describe('test query builder functionality', () => {
   it('basic test', () => {
     cy.visit('/login/e2e');
-    cy.get('#tokenInput').type(Cypress.env('GOOGLE_TOKEN'), { log: false, delay: 0 });
+    cy.get('#tokenInput', { timeout: 30000 }).type(Cypress.env('GOOGLE_TOKEN'), {
+      log: false,
+      delay: 0,
+    });
     cy.get('#e2eLoginButton').click();
     cy.contains('V2F_GWAS_Summary_Stats').should('be.visible');
     cy.contains('V2F_GWAS_Summary_Stats').click();
