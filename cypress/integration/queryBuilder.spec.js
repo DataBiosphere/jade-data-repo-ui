f566bbf6310ac0f9abefaed5b384cb6d8e8f9200
Michael Yessaillian <myessail@broadinstitute.org>
2020-03-23 15:59:39 -0400

    push new tests

--- cypress/integration/queryBuilder.spec.js
+++ cypress/integration/queryBuilder.spec.js
@@ -1,5 +1,22 @@
-describe('test query builder functionality', () => {
-  it('basic test', () => {
+describe('test query builder', () => {
+  // it('does render', () => {
+  //   cy.visit('/login/e2e');
+  //   cy.get('#tokenInput', { timeout: 30000 }).type(Cypress.env('GOOGLE_TOKEN'), {
+  //     log: false,
+  //     delay: 0,
+  //   });
+  //   cy.get('#e2eLoginButton').click();
+  //   cy.contains('V2F_GWAS_Summary_Stats').should('be.visible');
+  //   cy.contains('V2F_GWAS_Summary_Stats').click();
+  //   cy.get('p')
+  //     .contains('V2F_GWAS_Summary_Stats')
+  //     .should('be.visible');
+  //   cy.get('a')
+  //     .contains('query dataset')
+  //     .click();
+  // });
+
+  it('applies filters', () => {
     cy.visit('/login/e2e');
     cy.get('#tokenInput', { timeout: 30000 }).type(Cypress.env('GOOGLE_TOKEN'), {
       log: false,
@@ -11,8 +28,16 @@ describe('test query builder functionality', () => {
     cy.get('p')
       .contains('V2F_GWAS_Summary_Stats')
       .should('be.visible');
-    cy.get('a')
-      .contains('query dataset')
+    cy.get('a > .MuiButtonBase-root > .MuiButton-label').click();
+    cy.get('div.MuiButtonBase-root:nth-child(2) > svg:nth-child(1)').click();
+    cy.get('[data-cy=filterItem]')
+      .contains('ancestry')
+      .click();
+    cy.get(
+      '.MuiCollapse-entered > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > label:nth-child(1) > span:nth-child(1) > span:nth-child(1) > input:nth-child(1)',
+    ).click();
+    cy.get('[data-cy="applyFiltersButton"]')
+      .contains('Apply Filters')
       .click();
   });
 });
