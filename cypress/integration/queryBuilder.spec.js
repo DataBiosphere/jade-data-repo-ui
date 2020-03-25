5130fdb9151ae2eca3610f59df596f99357a96b3
Michael Yessaillian <myessail@broadinstitute.org>
2020-03-25 11:03:06 -0400

    trying this out

--- cypress/integration/queryBuilder.spec.js
+++ cypress/integration/queryBuilder.spec.js
@@ -1,20 +1,20 @@
 describe('test query builder', () => {
-  // it('does render', () => {
-  //   cy.visit('/login/e2e');
-  //   cy.get('#tokenInput', { timeout: 30000 }).type(Cypress.env('GOOGLE_TOKEN'), {
-  //     log: false,
-  //     delay: 0,
-  //   });
-  //   cy.get('#e2eLoginButton').click();
-  //   cy.contains('V2F_GWAS_Summary_Stats').should('be.visible');
-  //   cy.contains('V2F_GWAS_Summary_Stats').click();
-  //   cy.get('p')
-  //     .contains('V2F_GWAS_Summary_Stats')
-  //     .should('be.visible');
-  //   cy.get('a')
-  //     .contains('query dataset')
-  //     .click();
-  // });
+  it('does render', () => {
+    cy.visit('/login/e2e');
+    cy.get('#tokenInput', { timeout: 30000 }).type(Cypress.env('GOOGLE_TOKEN'), {
+      log: false,
+      delay: 0,
+    });
+    cy.get('#e2eLoginButton').click();
+    cy.contains('V2F_GWAS_Summary_Stats').should('be.visible');
+    cy.contains('V2F_GWAS_Summary_Stats').click();
+    cy.get('p')
+      .contains('V2F_GWAS_Summary_Stats')
+      .should('be.visible');
+    cy.get('a')
+      .contains('query dataset')
+      .click();
+  });
 
   it('applies filters', () => {
     cy.visit('/login/e2e');
