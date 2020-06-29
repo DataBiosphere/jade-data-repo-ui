a277fe834280d03c060911933926497857dcd5ea
MacKenzie Kerwin <52389456+mackenziekerwin@users.noreply.github.com>
2020-06-29 16:42:17 -0400

    DR-783 type decimals in range filter (#552)
    * converted range filters to strings
    
    * disable apply button for invalid numbers/ranges
    
    * fix validation
    
    * lower and upper bound on slider can be equal

--- src/components/dataset/query/sidebar/QueryViewSidebarItem.jsx
+++ src/components/dataset/query/sidebar/QueryViewSidebarItem.jsx
@@ -42,7 +42,7 @@ export class QueryViewSidebarItem extends React.PureComponent {
     const { filterMap } = this.state;
     // enable the button when there are unsaved changes
     if (!_.isEqual(prevState.filterMap, filterMap)) {
-      this.setState({ disableButton: _.isEmpty(filterMap.value) });
+      this.setState({ disableButton: this.invalidChange(filterMap) });
     }
     // disable the button when filters have just been applied
     if (!_.isEqual(prevProps.filterData, filterData)) {
@@ -69,6 +69,15 @@ export class QueryViewSidebarItem extends React.PureComponent {
   toggleExclude = (boxIsChecked) => {
     const { filterMap } = this.state;
     this.setState({ filterMap: { ...filterMap, exclude: boxIsChecked } });
+  };
+
+  invalidChange = (filterMap) => {
+    const { value } = filterMap;
+    if (filterMap.type === 'range') {
+      const badNumber = (number) => number === '' || number.endsWith('.') || isNaN(number);
+      return _.some(value, badNumber) || parseFloat(value[0]) > parseFloat(value[1]);
+    }
+    return _.isEmpty(value);
   };
 
   render() {
