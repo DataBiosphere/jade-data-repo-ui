89ca663d2cf8f5a4d139e9f7697a38e1c900f358
Michael Yessaillian <myessail@broadinstitute.org>
2020-07-08 12:23:50 -0400

    remove old stuff from hist module that breaks routing on chrome

--- src/modules/hist.js
+++ src/modules/hist.js
@@ -1,24 +1,6 @@
-// @flow
 import { createBrowserHistory } from 'history';
 
-import qs from 'qs';
-
 const history = createBrowserHistory();
-
-history.location = {
-  ...history.location,
-  query: qs.parse(history.location.search.substr(1)),
-  state: {},
-};
-
-history.listen(() => {
-  history.location = {
-    ...history.location,
-    query: qs.parse(history.location.search.substr(1)),
-    state: history.location.state || {},
-  };
-});
-
 const { go, goBack, push, replace } = history;
 
 export { go, goBack, push, replace };
