45d96816a8c7b339caec345d0c46e723cb27d4e1
nmalfroy <nmalfroy@broadinstitute.org>
2021-02-11 16:33:36 -0500

    Drive-by: Clear state on log out

--- src/reducers/snapshot.js
+++ src/reducers/snapshot.js
@@ -125,6 +125,7 @@ export default {
         immutable(state, {
           snapshotRequest: { $set: defaultSnapshotRequest },
         }),
+      [ActionTypes.USER_LOGOUT]: () => snapshotState,
     },
     snapshotState,
   ),
