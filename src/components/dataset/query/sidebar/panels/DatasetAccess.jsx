078b6c99e674af4fec70f41894816e4a113340e7
tlangs <tlangs@broadinstitute.org>
2022-01-26 16:29:43 -0500

    A working help screen

--- src/components/dataset/query/sidebar/panels/DatasetAccess.jsx
+++ src/components/dataset/query/sidebar/panels/DatasetAccess.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Link } from '@material-ui/core';
+import { Link, Typography } from '@material-ui/core';
 import UserList from '../../../../UserList';
 
 const styles = (theme) => ({
@@ -27,29 +27,76 @@ const styles = (theme) => ({
 export class DatasetAccess extends React.PureComponent {
   static propTypes = {
     classes: PropTypes.object,
+    helpOverlayToggle: PropTypes.func,
     policies: PropTypes.arrayOf(PropTypes.object),
   };
 
-  handleLearnMoreClick = () => {};
+  static helpTitle = (
+    <Typography variant="h4">Roles and memberships in Terra Data Repository</Typography>
+  );
+
+  static helpContent = (
+    <div>
+      <Typography variant="h6">Stewards</Typography>
+      <Typography>
+        Creating a dataset makes a user the Steward (or owner) of it. The steward of a dataset can:
+        <ul>
+          <li>Define the schema for a dataset</li>
+          <li>Delete a dataset (need to delete any resources leveraging the dataset first)</li>
+          <li>Assign new stewards, custodians and snapshot creators of a dataset</li>
+          <li>Add file/metadata to a dataset</li>
+          <li>Soft delete data (a function that hides data rather than permanently deletes it)</li>
+          <li>Read all the data in a dataset</li>
+          <li>Create a snapshot from a dataset</li>
+          <li>List all the stewards, custodians and snapshot creators of a dataset</li>
+        </ul>
+      </Typography>
+      <Typography variant="h6">Custodians</Typography>
+      <Typography>
+        A Steward can assign a Custodian to a dataset. The custodian of a dataset can:
+        <ul>
+          <li>Add file/metadata to a dataset</li>
+          <li>Soft delete data (a function that hides data rather than permanently deletes it)</li>
+          <li>Read all the data in a dataset</li>
+          <li>Create a snapshot from a dataset</li>
+          <li>List all the stewards, custodians and snapshot creators of a dataset</li>
+        </ul>
+      </Typography>
+      <Typography variant="h6">Snapshot Creator</Typography>
+      <Typography>
+        A Steward can assign a Snapshot Creator to a dataset. The Snapshot Creator of a dataset can:
+        <ul>
+          <li>Read all the data in a dataset</li>
+          <li>Create a snapshot from a dataset</li>
+          <li>List all the stewards, custodians and snapshot creators of a dataset</li>
+        </ul>
+      </Typography>
+    </div>
+  );
 
   render() {
-    const { classes, policies } = this.props;
+    const { classes, helpOverlayToggle, policies } = this.props;
+    const helpOverlayToggleWithContent = () =>
+      helpOverlayToggle(DatasetAccess.helpTitle, DatasetAccess.helpContent);
 
     const custodiansObj = policies.find((policy) => policy.name === 'custodian');
     const custodians = (custodiansObj && custodiansObj.members) || [];
-    const readersObj = policies.find((policy) => policy.name === 'snapshot_creator');
-    const readers = (readersObj && readersObj.members) || [];
+    const snapshotCreatorsObj = policies.find((policy) => policy.name === 'snapshot_creator');
+    const snapshotCreators = (snapshotCreatorsObj && snapshotCreatorsObj.members) || [];
     const stewardsObj = policies.find((policy) => policy.name === 'steward');
     const stewards = (stewardsObj && stewardsObj.members) || [];
 
     return (
       <div className={classes.root}>
         <div>
-          <Link className={classes.blueLink}>Learn more</Link> about roles and memberships
+          <Link href="#" className={classes.blueLink} onClick={helpOverlayToggleWithContent}>
+            Learn more
+          </Link>{' '}
+          about roles and memberships
         </div>
         <UserList users={custodians} typeOfUsers="Stewards" canManageUsers={false} />
         <UserList users={stewards} typeOfUsers="Custodians" canManageUsers={false} />
-        <UserList users={readers} typeOfUsers="Readers" canManageUsers={false} />
+        <UserList users={snapshotCreators} typeOfUsers="Snapshot Creators" canManageUsers={false} />
       </div>
     );
   }
--- src/components/dataset/query/sidebar/panels/InfoView.jsx
+++ src/components/dataset/query/sidebar/panels/InfoView.jsx
@@ -6,7 +6,8 @@ import { connect } from 'react-redux';
 import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import { ExpandMore } from '@material-ui/icons';
+import { Close, ExpandMore } from '@material-ui/icons';
+import IconButton from '@material-ui/core/IconButton';
 import DatasetAccess from './DatasetAccess';
 
 const styles = (theme) => ({
@@ -15,67 +16,121 @@ const styles = (theme) => ({
     margin: theme.spacing(1),
     padding: theme.spacing(2),
   },
-  blueHeader: {
+  membershipAccordionContainer: {
+    margin: theme.spacing(1),
+    padding: theme.spacing(2),
+  },
+  membershipAccordion: {
     fontSize: '14px',
     lineHeight: '22px',
     fontWeight: '600',
     color: theme.palette.common.link,
   },
+  helpOverlay: {
+    position: 'absolute',
+    backgroundColor: theme.palette.primary.light,
+    width: '100%',
+    height: '100%',
+    zIndex: '1',
+  },
+  helpOverlayCloseButton: {
+    float: 'right',
+    marginRight: theme.spacing(4),
+    color: theme.palette.common.link,
+  },
 });
 
 export class InfoView extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHelpVisible: false,
+    };
+    this.toggleHelpOverlay = this.toggleHelpOverlay.bind(this);
+  }
+
   static propTypes = {
     classes: PropTypes.object,
     dataset: PropTypes.object,
     datasetPolicies: PropTypes.array,
   };
 
+  toggleHelpOverlay(helpTitle, helpContent) {
+    const { isHelpVisible } = this.state;
+    this.setState({
+      isHelpVisible: !isHelpVisible,
+      helpTitle,
+      helpContent,
+    });
+  }
+
   render() {
     const { classes, dataset, datasetPolicies } = this.props;
-
+    const { isHelpVisible, helpTitle, helpContent } = this.state;
     return (
       <div className={classes.root}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h5">Dataset Overview</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h5">{dataset.name}</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h6">About this dataset:</Typography>
-            <Typography>{dataset.description}</Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h6">Storage</Typography>
-            <Typography>
-              {dataset.storage.map((storageResource) => (
-                <div>
-                  {storageResource.cloudResource}: {storageResource.region}
-                </div>
-              ))}
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h6">Date Created:</Typography>
-            <Typography>{moment(dataset.createdDate).fromNow()}</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Accordion>
-              <AccordionSummary
-                className={clsx(classes.blueHeader)}
-                expandIcon={<ExpandMore />}
-                aria-controls="memberships-content"
-                id="memberships-header"
+        {isHelpVisible && (
+          <Grid container spacing={2}>
+            <Grid item xs={11}>
+              {helpTitle}
+            </Grid>
+            <Grid item xs={1}>
+              <IconButton
+                className={classes.helpOverlayCloseButton}
+                onClick={this.toggleHelpOverlay}
               >
-                Summary of roles and memberships
-              </AccordionSummary>
-              <AccordionDetails>
-                <DatasetAccess policies={datasetPolicies} />
-              </AccordionDetails>
-            </Accordion>
+                <Close />
+              </IconButton>
+            </Grid>
+            <Grid item xs={11}>
+              {helpContent}
+            </Grid>
           </Grid>
-        </Grid>
+        )}
+        {!isHelpVisible && (
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h4">Dataset Overview</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h5">{dataset.name}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6">About this dataset:</Typography>
+              <Typography>{dataset.description}</Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h6">Storage</Typography>
+              {dataset.storage.map((storageResource, i) => (
+                <Typography key={i}>
+                  {storageResource.cloudResource}: {storageResource.region}
+                </Typography>
+              ))}
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h6">Date Created:</Typography>
+              <Typography>{moment(dataset.createdDate).fromNow()}</Typography>
+            </Grid>
+            <Grid item xs={12} className={classes.membershipAccordionContainer}>
+              <Accordion>
+                <AccordionSummary
+                  className={clsx(classes.membershipAccordion)}
+                  expandIcon={<ExpandMore />}
+                  aria-controls="memberships-content"
+                  id="memberships-header"
+                >
+                  Summary of roles and memberships
+                </AccordionSummary>
+                <AccordionDetails>
+                  <DatasetAccess
+                    policies={datasetPolicies}
+                    helpOverlayToggle={this.toggleHelpOverlay}
+                  />
+                </AccordionDetails>
+              </Accordion>
+            </Grid>
+          </Grid>
+        )}
       </div>
     );
   }
