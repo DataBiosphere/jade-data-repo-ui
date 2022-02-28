8c5c1bf7564ef5896aaf734df5747c8c89f1084c
tlangs <tlangs@broadinstitute.org>
2022-02-28 17:36:49 -0500

    back out cypress changes from previous branch

--- cypress/integration/errors.spec.js
+++ cypress/integration/errors.spec.js
@@ -28,8 +28,6 @@ describe('test error handling', () => {
       },
     }).as('getQueryResults');
 
-    cy.get('a > .MuiButtonBase-root').click();
-
     cy.wait(['@getDataset', '@getDatasetPolicies', '@getQueryResults']);
     cy.contains('Error 401: This is the reason for the error').should('be.visible');
   });
@@ -45,8 +43,6 @@ describe('test error handling', () => {
       },
     }).as('getQueryResults');
 
-    cy.get('a > .MuiButtonBase-root').click();
-
     cy.wait(['@getDataset', '@getDatasetPolicies', '@getQueryResults']);
     cy.contains('Error 401: Was not able to query').should('be.visible');
   });
@@ -60,8 +56,6 @@ describe('test error handling', () => {
         message: 'Was not able to query',
       },
     }).as('getQueryResults');
-
-    cy.get('a > .MuiButtonBase-root').click();
 
     cy.wait(['@getDataset', '@getDatasetPolicies', '@getQueryResults']);
     cy.contains('Error 401: Was not able to query').should('be.visible');
@@ -80,8 +74,6 @@ describe('test error handling', () => {
       status: 200,
       response: { jobComplete: false, jobReference: { jobId: 'jobId' } },
     }).as('getQueryJobResults');
-
-    cy.get('a > .MuiButtonBase-root').click();
 
     cy.wait(['@getDataset', '@getDatasetPolicies', '@getQueryResults', '@getQueryJobResults']);
     cy.contains(
