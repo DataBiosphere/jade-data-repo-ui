d8f56c57df7571aef2adf05d8bea6766c7565dea
Shelby Holden <sholden@broadinstitute.org>
2022-03-10 15:00:36 -0500

    Move query view UI code into common

---
+++ src/components/common/query/QueryView.jsx
@@ -0,0 +1,109 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+import { Button, Typography } from '@material-ui/core';
+
+import { Link } from 'react-router-dom';
+import SidebarDrawer from './sidebar/SidebarDrawer';
+import QueryViewDropdown from './QueryViewDropdown';
+import JadeTable from '../../table/JadeTable';
+import SnapshotPopup from '../../snapshot/SnapshotPopup';
+
+const styles = (theme) => ({
+  wrapper: {
+    paddingTop: theme.spacing(0),
+    padding: theme.spacing(4),
+  },
+  scrollTable: {
+    overflowY: 'auto',
+    height: '100%',
+  },
+  headerArea: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+});
+
+function QueryView({
+  classes,
+  resourceLoaded,
+  resourceName,
+  tableNames,
+  handleChange,
+  queryResults,
+  selected,
+  selectedTable,
+  canLink,
+  panels,
+  handleDrawerWidth,
+  sidebarWidth,
+}) {
+  return (
+    <Fragment>
+      {resourceLoaded && (
+        <Fragment>
+          <Grid container spacing={0} className={classes.wrapper}>
+            <Grid container spacing={1}>
+              <Grid item xs={12}>
+                <Typography variant="h3" className={classes.headerArea}>
+                  {resourceName}
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <QueryViewDropdown options={tableNames} onSelectedItem={handleChange} />
+              </Grid>
+              <Grid item xs={3}>
+                <Link to="overview">
+                  <Button
+                    className={classes.viewDatasetButton}
+                    color="primary"
+                    variant="outlined"
+                    disableElevation
+                    size="large"
+                  >
+                    Back to Overview
+                  </Button>
+                </Link>
+              </Grid>
+            </Grid>
+            <Grid container spacing={0}>
+              <Grid item xs={11}>
+                <div className={classes.scrollTable}>
+                  <JadeTable queryResults={queryResults} title={selected} table={selectedTable} />
+                </div>
+              </Grid>
+            </Grid>
+          </Grid>
+          <SidebarDrawer
+            canLink={canLink}
+            panels={panels}
+            handleDrawerWidth={handleDrawerWidth}
+            width={sidebarWidth}
+            table={selectedTable}
+            selected={selected}
+          />
+          <SnapshotPopup />
+        </Fragment>
+      )}
+    </Fragment>
+  );
+}
+
+QueryView.propTypes = {
+  classes: PropTypes.object,
+  resourceLoaded: PropTypes.bool,
+  resourceName: PropTypes.string,
+  tableNames: PropTypes.array,
+  handleChange: PropTypes.func,
+  queryResults: PropTypes.object,
+  selected: PropTypes.string,
+  selectedTable: PropTypes.object,
+  canLink: PropTypes.bool,
+  panels: PropTypes.object,
+  handleDrawerWidth: PropTypes.func,
+  sidebarWidth: PropTypes.number,
+};
+
+export default withStyles(styles)(QueryView);
