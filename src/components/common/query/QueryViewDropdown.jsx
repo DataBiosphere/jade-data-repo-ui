4c6bbe0a4cdc9b2eebed6813136ad4eb5131e826
Shelby Holden <sholden@broadinstitute.org>
2022-03-21 03:54:00 -0400

    Switch QueryViewDrop to be functional component

--- src/components/common/query/QueryViewDropdown.jsx
+++ src/components/common/query/QueryViewDropdown.jsx
@@ -1,41 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import JadeDropdown from '../../dataset/query/JadeDropdown';
 
-export class QueryViewDropdown extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    const { onSelectedItem, options } = this.props;
-    this.state = {
-      table: options[0],
-    };
-    onSelectedItem(options[0]);
-  }
+function QueryViewDropdown({ onSelectedItem, options }) {
+  const [table, setTable] = useState(options[0]);
 
-  static propTypes = {
-    onSelectedItem: PropTypes.func,
-    options: PropTypes.array,
+  useEffect(() => {
+    onSelectedItem(table);
+  }, [table, onSelectedItem]);
+
+  const handleChange = (event) => {
+    setTable(event.target.value);
   };
 
-  handleChange = (event) => {
-    const { onSelectedItem } = this.props;
-    this.setState({ table: event.target.value });
-    onSelectedItem(event.target.value);
-  };
-
-  render() {
-    const { options } = this.props;
-    const { table } = this.state;
-
-    return (
-      <JadeDropdown
-        onSelectedItem={this.handleChange}
-        options={options}
-        value={table}
-        name="Select Table"
-      />
-    );
-  }
+  return (
+    <JadeDropdown
+      onSelectedItem={handleChange}
+      options={options}
+      value={table}
+      name="Select Table"
+    />
+  );
 }
+
+QueryViewDropdown.propTypes = {
+  onSelectedItem: PropTypes.func,
+  options: PropTypes.array,
+};
 
 export default QueryViewDropdown;
