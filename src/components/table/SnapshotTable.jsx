f183256ded4d2b31ad9322a86f222f2df3ff3170
tlangs <tlangs@broadinstitute.org>
2022-03-29 12:53:52 -0400

    Functionalize snapshot table component

--- src/components/table/SnapshotTable.jsx
+++ src/components/table/SnapshotTable.jsx
@@ -13,78 +13,75 @@ const styles = (theme) => ({
   },
 });
 
-class SnapshotTable extends React.PureComponent {
-  static propTypes = {
-    classes: PropTypes.object.isRequired,
-    filteredSnapshotCount: PropTypes.number,
-    handleFilterSnapshots: PropTypes.func,
-    searchString: PropTypes.string,
-    snapshotCount: PropTypes.number,
-    snapshots: PropTypes.array.isRequired,
-    summary: PropTypes.bool,
-  };
-
-  render() {
-    const {
-      classes,
-      handleFilterSnapshots,
-      snapshotCount,
-      filteredSnapshotCount,
-      snapshots,
-      summary,
-      searchString,
-    } = this.props;
-    // TODO add back modified_date column
-    const columns = [
-      {
-        label: 'Snapshot Name',
-        name: 'name',
-        allowSort: true,
-        render: (row) => (
-          <Link to={`/snapshots/${row.id}`} className={classes.jadeLink}>
-            {row.name}
-          </Link>
-        ),
-      },
-      {
-        label: 'Description',
-        name: 'description',
-        allowSort: true,
-      },
-      {
-        label: 'Date created',
-        name: 'created_date',
-        allowSort: true,
-        render: (row) => moment(row.createdDate).fromNow(),
-      },
-      {
-        label: 'Storage Regions',
-        name: 'storage',
-        allowSort: false,
-        render: (row) => Array.from(new Set(row.storage.map((s) => s.region))).join(', '),
-      },
-      {
-        label: 'Cloud Platform',
-        name: 'platform',
-        allowSort: false,
-        render: renderCloudPlatforms,
-      },
-    ];
-    return (
-      <div>
-        <LightTable
-          columns={columns}
-          handleEnumeration={handleFilterSnapshots}
-          itemType="snapshots"
-          rows={snapshots}
-          summary={summary}
-          totalCount={snapshotCount}
-          filteredCount={filteredSnapshotCount}
-          searchString={searchString}
-        />
-      </div>
-    );
-  }
+function SnapshotTable({
+  classes,
+  filteredSnapshotCount,
+  handleFilterSnapshots,
+  searchString,
+  snapshotCount,
+  snapshots,
+  summary,
+}) {
+  // TODO add back modified_date column
+  const columns = [
+    {
+      label: 'Snapshot Name',
+      name: 'name',
+      allowSort: true,
+      render: (row) => (
+        <Link to={`/snapshots/${row.id}`} className={classes.jadeLink}>
+          {row.name}
+        </Link>
+      ),
+    },
+    {
+      label: 'Description',
+      name: 'description',
+      allowSort: true,
+    },
+    {
+      label: 'Date created',
+      name: 'created_date',
+      allowSort: true,
+      render: (row) => moment(row.createdDate).fromNow(),
+    },
+    {
+      label: 'Storage Regions',
+      name: 'storage',
+      allowSort: false,
+      render: (row) => Array.from(new Set(row.storage.map((s) => s.region))).join(', '),
+    },
+    {
+      label: 'Cloud Platform',
+      name: 'platform',
+      allowSort: false,
+      render: renderCloudPlatforms,
+    },
+  ];
+  return (
+    <div>
+      <LightTable
+        columns={columns}
+        handleEnumeration={handleFilterSnapshots}
+        itemType="snapshots"
+        rows={snapshots}
+        summary={summary}
+        totalCount={snapshotCount}
+        filteredCount={filteredSnapshotCount}
+        searchString={searchString}
+      />
+    </div>
+  );
 }
+
+SnapshotTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+  filteredSnapshotCount: PropTypes.number,
+  handleFilterSnapshots: PropTypes.func,
+  searchString: PropTypes.string,
+  snapshotCount: PropTypes.number,
+  snapshots: PropTypes.array.isRequired,
+  summary: PropTypes.bool,
+};
 
 export default withStyles(styles)(SnapshotTable);
