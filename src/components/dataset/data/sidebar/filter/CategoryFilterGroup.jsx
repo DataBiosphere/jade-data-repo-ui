d15bd42af095e2c2c7debe560af9b638efc1d45f
Shelby Holden <sholden@broadinstitute.org>
2022-06-07 11:03:18 -0400

    CategoryFilterGroup - convert to functional

--- src/components/dataset/data/sidebar/filter/CategoryFilterGroup.jsx
+++ src/components/dataset/data/sidebar/filter/CategoryFilterGroup.jsx
@@ -1,69 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
 import { CategoryFilter } from './CategoryFilter';
 
-export class CategoryFilterGroup extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: {},
-    };
-  }
+function CategoryFilterGroup({ column, filterMap, handleChange, originalValues, table, values }) {
+  const [selected, setSelected] = useState({});
 
-  static propTypes = {
-    column: PropTypes.object,
-    filterMap: PropTypes.object,
-    handleChange: PropTypes.func,
-    originalValues: PropTypes.object,
-    table: PropTypes.string,
-    values: PropTypes.object,
-  };
+  useEffect(() => {
+    setSelected(_.get(filterMap, 'value', {}));
+  }, [filterMap]);
 
-  componentDidUpdate(prevProps) {
-    const { filterMap } = this.props;
-    if (!_.isEqual(filterMap, prevProps.filterMap)) {
-      const selected = _.get(filterMap, 'value', {});
-      this.setState({ selected });
-    }
-  }
-
-  handleChange = (box) => {
-    const { handleChange } = this.props;
-    const { selected } = this.state;
+  const handleFilterChange = (box) => {
     const selectedClone = _.clone(selected);
     if (box.value) {
       selectedClone[box.name] = box.value;
-      this.setState({
-        selected: selectedClone,
-      });
+      setSelected(selectedClone);
     } else {
       delete selectedClone[box.name];
-      this.setState({ selected: selectedClone });
+      setSelected(selectedClone);
     }
     handleChange(selectedClone);
   };
 
-  render() {
-    const { column, values, table, originalValues, filterMap } = this.props;
-    const checkboxes = _.keys(originalValues).map((name) => {
-      const count = parseInt(_.get(values, name, '0'), 10);
-      return (
-        <div key={name}>
-          <CategoryFilter
-            column={column}
-            filterMap={filterMap}
-            handleChange={this.handleChange}
-            name={name}
-            count={count}
-            table={table}
-          />
-        </div>
-      );
-    });
+  const checkboxes = _.keys(originalValues).map((name) => {
+    const count = parseInt(_.get(values, name, '0'), 10);
+    return (
+      <div key={name}>
+        <CategoryFilter
+          column={column}
+          filterMap={filterMap}
+          handleChange={handleFilterChange}
+          name={name}
+          count={count}
+          table={table}
+        />
+      </div>
+    );
+  });
 
-    return <div>{checkboxes}</div>;
-  }
+  return <div>{checkboxes}</div>;
 }
+
+CategoryFilterGroup.propTypes = {
+  column: PropTypes.object,
+  filterMap: PropTypes.object,
+  handleChange: PropTypes.func,
+  originalValues: PropTypes.object,
+  table: PropTypes.string,
+  values: PropTypes.object,
+};
 
 export default CategoryFilterGroup;
