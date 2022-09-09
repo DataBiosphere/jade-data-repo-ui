ff67d47b73fc3b174dc44b7fde95d426b481e7c6
Elliot Otchet <otchet@broadinstitute.org>
2022-09-09 09:19:15 -0400

    Incorporates styling and behaviors feedback from UX.

--- src/components/DescriptionView.jsx
+++ src/components/DescriptionView.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TextField, IconButton, Typography } from '@mui/material';
-import InputAdornment from '@mui/material/InputAdornment';
-import Pencil from '@mui/icons-material/Edit';
+import { Button, Box, Grid, IconButton, TextField, Typography } from '@mui/material';
+import Edit from '@mui/icons-material/Edit';
+import { showNotification } from 'modules/notifications';
 import UndoIcon from '@mui/icons-material/Undo';
 import SaveIcon from '@mui/icons-material/Save';
 import { withStyles } from '@mui/styles';
@@ -10,9 +10,41 @@ import { patchDatasetDescription } from 'actions';
 import { DatasetRoles } from '../constants';
 
 const styles = (theme) => ({
+  root: {
+    width: '100%',
+    display: 'flex',
+  },
+  iconDiv: {
+    width: '2%',
+  },
+  textInputDiv: {
+    width: '100%',
+  },
   descriptionInput: {
-    '&.Mui-focused': {
-      backgroundColor: 'white',
+    backgroundColor: 'white',
+  },
+  descriptionButtons: {
+    margin: theme.spacing(1),
+  },
+  editIconButton: {
+    border: `1px solid ${theme.palette.primary.main}`,
+    borderRadius: '3px',
+    padding: '1px',
+    backgroundColor: theme.palette.primary.main,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.hover,
+    },
+  },
+  editIconImage: {
+    fontSize: theme.typography.h6.fontSize,
+    color: theme.palette.primary.light,
+  },
+  undoButton: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.grey[500],
+    '&:hover': {
+      backgroundColor: theme.palette.primary.light,
     },
   },
 });
@@ -23,7 +55,10 @@ class DescriptionView extends React.PureComponent {
     this.state = {
       hasDescriptionChanged: false,
       descriptionValue: descriptionInitialValue,
+      isEditing: false,
+      isPendingSave: false,
     };
+    this.textFieldRef = React.createRef();
   }
 
   static propTypes = {
@@ -36,31 +71,71 @@ class DescriptionView extends React.PureComponent {
 
   componentDidUpdate(prevState) {
     const { description } = this.props;
-    const { descriptionValue } = this.state;
-    if (description === descriptionValue) {
+    const { descriptionValue, isPendingSave } = this.state;
+    if (isPendingSave && description === descriptionValue) {
       this.setState({ hasDescriptionChanged: false });
+      this.setState({ isPendingSave: false });
+      this.onExitEdit();
     }
   }
 
   descriptionChanged(newDescription, originalDescription) {
+    if (newDescription.length > 2047) {
+      showNotification(
+        JSON.parse(
+          '{"response":{"status":"Description too long","data":{"message":"","errorDetail":"Exceeds 2047 characters.  Please revise and save again."}}}',
+        ),
+      );
+      this.setState({ descriptionValue: newDescription.substring(0, 2046) });
+    } else {
+      this.setState({ descriptionValue: newDescription });
+    }
     if (newDescription !== originalDescription) {
       this.setState({ hasDescriptionChanged: true });
     } else {
       this.setState({ hasDescriptionChanged: false });
     }
-    this.setState({ descriptionValue: newDescription });
+  }
+
+  onDescriptionTextBlur(newDescription, originalDescription) {
+    this.descriptionChanged(newDescription, originalDescription);
+    this.onExitEdit();
+  }
+
+  onDescriptionTextClick() {
+    const { isEditing } = this.state;
+    if (!isEditing) {
+      this.setState({ isEditing: true });
+    }
+  }
+
+  onEditClick() {
+    const { isEditing } = this.state;
+    if (!isEditing) {
+      this.textFieldRef.current.focus();
+      this.setState({ isEditing: true });
+    }
+  }
+
+  onExitEdit() {
+    const { description } = this.props;
+    const { descriptionValue, isPendingSave } = this.state;
+    if (description === descriptionValue) {
+      this.setState({ isEditing: false });
+    }
   }
 
   onSaveClick(descriptionText) {
     const { updateDescriptionFn } = this.props;
+    this.setState({ isPendingSave: true });
     updateDescriptionFn(descriptionText);
   }
 
   render() {
     const { classes, description, userRoles, datasetDescription } = this.props;
-    const { hasDescriptionChanged, descriptionValue } = this.state;
+    const { hasDescriptionChanged, descriptionValue, isEditing } = this.state;
     const canEdit = userRoles.includes(DatasetRoles.STEWARD);
-
+    // const canEdit = false;
     return (
       <>
         {!canEdit && (
@@ -69,48 +144,66 @@ class DescriptionView extends React.PureComponent {
           </Typography>
         )}
         {canEdit && (
-          <div>
-            <TextField
-              fullWidth={true}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment style={{ pointerEvents: 'none' }} position="start">
-                    <Pencil titleAccess="Pencil icon indicating the description is editable." />
-                  </InputAdornment>
-                ),
-                className: classes.descriptionInput,
-              }}
-              multiline={true}
-              maxRows={5}
-              minRows={2}
-              onChange={(e) => this.descriptionChanged(e.target.value, description)}
-              onBlur={(e) => this.descriptionChanged(e.target.value, description)}
-              placeholder="Add a description."
-              value={descriptionValue}
-              variant="outlined"
-            />
-            <IconButton
-              aria-label="Save description changes"
-              color="primary"
-              disabled={!hasDescriptionChanged}
-              size="small"
-              type="button"
-              variant="contained"
-              onClick={() => this.onSaveClick(descriptionValue)}
-            >
-              <SaveIcon titleAccess="Save description changes" />
-            </IconButton>
-            <IconButton
-              aria-label="Undo description changes"
-              color="primary"
-              disabled={!hasDescriptionChanged}
-              onClick={() => this.descriptionChanged(description, description)}
-              size="small"
-              type="button"
-              variant="contained"
-            >
-              <UndoIcon titleAccess="Undo description changes" />
-            </IconButton>
+          <div className={classes.root}>
+            <div className={classes.iconDiv}>
+              <IconButton
+                className={classes.editIconButton}
+                aria-label="Edit description"
+                onClick={(e) => this.onEditClick()}
+                disableFocusRipple={true}
+                disableRipple={true}
+              >
+                <Edit fontStyle="small" className={classes.editIconImage} />
+              </IconButton>
+            </div>
+            <div className={classes.textInputDiv}>
+              <TextField
+                fullWidth={true}
+                inputRef={this.textFieldRef}
+                maxLength={2047}
+                multiline={true}
+                maxRows={5}
+                minRows={2}
+                onFocus={() => this.onDescriptionTextClick()}
+                onClick={() => this.onDescriptionTextClick()}
+                onChange={(e) => this.descriptionChanged(e.target.value, description)}
+                onBlur={(e) => this.onDescriptionTextBlur(e.target.value, description)}
+                placeholder="Add a description."
+                type="text"
+                value={descriptionValue}
+                variant="outlined"
+                InputProps={
+                  isEditing
+                    ? {
+                        className: classes.descriptionInput,
+                      }
+                    : {}
+                }
+              />
+              {isEditing && (
+                <>
+                  <Button
+                    aria-label="Save description changes"
+                    className={classes.descriptionButtons}
+                    color="primary"
+                    disabled={!hasDescriptionChanged}
+                    variant="contained"
+                    onClick={() => this.onSaveClick(descriptionValue)}
+                  >
+                    SAVE
+                  </Button>
+                  <Button
+                    aria-label="Undo description changes"
+                    className={classes.undoButton}
+                    disabled={!hasDescriptionChanged}
+                    onClick={() => this.descriptionChanged(description, description)}
+                    variant="contained"
+                  >
+                    UNDO
+                  </Button>
+                </>
+              )}
+            </div>
           </div>
         )}
       </>
