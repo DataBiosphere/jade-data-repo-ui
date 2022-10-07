ffa6a9d55d712ef79a8a617365682abd30723805
clu@asymmetrik.com <clu@asymmetrik.com>
2022-10-07 15:18:12 -0400

    [DR-2741] fixing build?

--- src/components/snapshot/overview/SnapshotOverviewPanel.tsx
+++ src/components/snapshot/overview/SnapshotOverviewPanel.tsx
@@ -165,7 +165,7 @@ function SnapshotOverviewPanel(props: SnapshotOverviewPanelProps) {
       <TabPanel value={value} index={2}>
         <Grid container spacing={2}>
           <Grid item xs={9}>
-            <SnapshotAccess horizontal={false} showHelp={true} />
+            <SnapshotAccess horizontal={false} />
           </Grid>
         </Grid>
       </TabPanel>
