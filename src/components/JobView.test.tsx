3be5147d78a4051bf00f216e55584b06475bcbaa
clu@asymmetrik.com <clu@asymmetrik.com>
2022-10-18 20:14:44 -0400

    [DR-2789] added rendering tests for the different result states

--- src/components/JobView.test.tsx
+++ src/components/JobView.test.tsx
@@ -8,14 +8,41 @@ import _ from 'lodash';
 import JobView from './JobView';
 import globalTheme from '../modules/theme';
 import history from '../modules/hist';
-import { initialJobState } from 'reducers/job';
 import { initialUserState } from 'reducers/user';
 import { initialQueryState } from 'reducers/query';
 import { JobModelJobStatusEnum } from 'generated/tdr';
 
+const testDate = new Date();
 const initialState = {
   searchString: '',
-  jobs: _.cloneDeep(initialJobState),
+  jobs: {
+    jobs: [
+      {
+        id: 'testingId',
+        job_status: JobModelJobStatusEnum.Failed,
+        status_code: 200,
+        description: 'the first pancake always burns',
+        class_name: 'this.is.fake.class',
+        submitted: testDate.toISOString(),
+      },
+      {
+        id: 'testingId2',
+        job_status: JobModelJobStatusEnum.Succeeded,
+        status_code: 200,
+        description: 'Ingest from source.json to ArraysInputsTable in dataset id (uuid)',
+        class_name: 'this.is.fake.class2',
+        submitted: (new Date(testDate.valueOf() - 2 * 60000)).toISOString(),
+      },
+      {
+        id: 'testingId3',
+        job_status: JobModelJobStatusEnum.Running,
+        status_code: 200,
+        description: 'Running',
+        class_name: 'this.is.fake.class2',
+        submitted: (new Date(testDate.valueOf() - 2 * 120000)).toISOString(),
+      },
+    ],
+  },
   user: _.cloneDeep(initialUserState),
   query: _.cloneDeep(initialQueryState),
 };
@@ -48,17 +75,35 @@ describe('JobView', () => {
   });
 
   it('should make a request for jobs', () => {
-
+    // IT DOES NOT DO THIS AT ALL
+    cy.get('tbody tr').should('have.length', initialState.jobs.jobs.length);
   });
 
-  it('should show the jobs', () => {
-    initialState.jobs.jobs = [
-      {
-        id: 'testingId',
-        job_status: JobModelJobStatusEnum.Succeeded,
-        status_code: 200,
-      }
-    ];
+  it('should show successful jobs', () => {
+    cy.get('tbody tr').eq(0).children().should('have.length', 5);
+    cy.get('tbody tr').eq(0).children().eq(0).should('have.text', initialState.jobs.jobs[0].id);
+    cy.get('tbody tr').eq(0).children().eq(1).should('have.text', 'class');
+    cy.get('tbody tr').eq(0).children().eq(2).should('have.text', initialState.jobs.jobs[0].description);
+    cy.get('tbody tr').eq(0).children().eq(3).should('have.text', 'a few seconds ago');
+    cy.get('tbody tr').eq(0).children().eq(4).should('have.text', 'Failed');
+  });
+
+  it('should show failed jobs', () => {
+    cy.get('tbody tr').eq(1).children().should('have.length', 5);
+    cy.get('tbody tr').eq(1).children().eq(0).should('have.text', initialState.jobs.jobs[1].id);
+    cy.get('tbody tr').eq(1).children().eq(1).should('have.text', 'class2');
+    cy.get('tbody tr').eq(1).children().eq(2).should('have.text', initialState.jobs.jobs[1].description);
+    cy.get('tbody tr').eq(1).children().eq(3).should('have.text', '2 minutes ago');
+    cy.get('tbody tr').eq(1).children().eq(4).should('have.text', 'Completed');
+  });
+
+  it('should show in progress jobs', () => {
+    cy.get('tbody tr').eq(2).children().should('have.length', 5);
+    cy.get('tbody tr').eq(2).children().eq(0).should('have.text', initialState.jobs.jobs[2].id);
+    cy.get('tbody tr').eq(2).children().eq(1).should('have.text', 'class2');
+    cy.get('tbody tr').eq(2).children().eq(2).should('have.text', initialState.jobs.jobs[2].description);
+    cy.get('tbody tr').eq(2).children().eq(3).should('have.text', '4 minutes ago');
+    cy.get('tbody tr').eq(2).children().eq(4).should('have.text', 'In Progress');
   });
 
 });
