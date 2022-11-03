c2dbfb8c5a6c1f59c955b27d91a6c1e60e04b0fe
clu@asymmetrik.com <clu@asymmetrik.com>
2022-11-03 12:37:53 -0400

    Migrating cypress (cypress.json) to new cypress (cypress.config). These are the results that are automatically generated, follow on work will be necessary to confirm this works for our code and our tests. Minimal testing shows component testing is not finding our tests anymore.

---
+++ cypress.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "e6ttjx",
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  defaultCommandTimeout: 60000,
+  requestTimeout: 120000,
+
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+  },
+
+  component: {
+    setupNodeEvents(on, config) {},
+    specPattern: "src/**/*test.{js,jsx,ts,tsx}",
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
