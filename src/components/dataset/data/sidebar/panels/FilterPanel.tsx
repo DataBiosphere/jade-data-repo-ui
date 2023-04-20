46824d226c300b967d26c951de994384804117c3
Shelby Holden <sholden@broadinstitute.org>
2023-04-20 11:18:39 -0400

    Typescriptify + Functional Component: DataViewSidebarItem
    prettier DataViewSidebarItem
    
    rework DataViewSidebarItem
    
    prettier

--- src/components/dataset/data/sidebar/panels/FilterPanel.tsx
+++ src/components/dataset/data/sidebar/panels/FilterPanel.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 import clsx from 'clsx';
 import { connect } from 'react-redux';
-import { WithStyles, withStyles,ClassNameMap, createStyles } from '@mui/styles';
+import { WithStyles, withStyles, ClassNameMap, createStyles } from '@mui/styles';
 import { CustomTheme } from '@mui/material/styles';
 import { DatasetModel, TableModel, ColumnModel } from 'generated/tdr';
 import {
@@ -24,68 +24,69 @@ import { TdrState } from '../../../../../reducers';
 import { applyFilters } from '../../../../../actions';
 import { AppDispatch } from '../../../../../store';
 
-const styles = (theme: CustomTheme) => createStyles({
-  root: {
-    margin: theme.spacing(1),
-    display: 'flex',
-    flexDirection: 'column',
-    position: 'absolute',
-    top: '0px',
-    right: '0px',
-    bottom: '0px',
-    left: '0px',
-  },
-  hide: {
-    display: 'none',
-  },
-  filterPanel: {
-    marginBottom: '8px',
-  },
-  sidebarTitle: {
-    flexDirection: 'column',
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  snapshotBtnCntnr: {
-    textAlign: 'end',
-  },
-  panelContent: {
-    padding: `0px ${theme.spacing(2)}`,
-  },
-  rowOne: {
-    flex: 1,
-    overflowY: 'auto',
-    overflowX: 'hidden',
-  },
-  rowTwo: {},
-  searchBar: {
-    display: 'flex',
-    alignItems: 'center',
-    borderRadius: theme.shape.borderRadius,
-  },
-  inputBase: {
-    width: '100%',
-  },
-  filterListItem: {
-    justifyContent: 'space-between',
-    fontWeight: '500',
-    '&:hover': {
-      backgroundColor: 'transparent',
+const styles = (theme: CustomTheme) =>
+  createStyles({
+    root: {
+      margin: theme.spacing(1),
+      display: 'flex',
+      flexDirection: 'column',
+      position: 'absolute',
+      top: '0px',
+      right: '0px',
+      bottom: '0px',
+      left: '0px',
     },
-  },
-  highlighted: {
-    backgroundColor: theme.palette.primary.focus,
-    borderRadius: theme.shape.borderRadius,
-    paddingBottom: theme.spacing(0.5),
-  },
-  tooltip: {
-    pointerEvents: 'auto !important' as any, // TS doesn't recognize !important as PointerEvent type
-    color: 'red',
-  },
-  button: {
-    marginTop: theme.spacing(1),
-  },
-});
+    hide: {
+      display: 'none',
+    },
+    filterPanel: {
+      marginBottom: '8px',
+    },
+    sidebarTitle: {
+      flexDirection: 'column',
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    snapshotBtnCntnr: {
+      textAlign: 'end',
+    },
+    panelContent: {
+      padding: `0px ${theme.spacing(2)}`,
+    },
+    rowOne: {
+      flex: 1,
+      overflowY: 'auto',
+      overflowX: 'hidden',
+    },
+    rowTwo: {},
+    searchBar: {
+      display: 'flex',
+      alignItems: 'center',
+      borderRadius: theme.shape.borderRadius,
+    },
+    inputBase: {
+      width: '100%',
+    },
+    filterListItem: {
+      justifyContent: 'space-between',
+      fontWeight: '500',
+      '&:hover': {
+        backgroundColor: 'transparent',
+      },
+    },
+    highlighted: {
+      backgroundColor: theme.palette.primary.focus,
+      borderRadius: theme.shape.borderRadius,
+      paddingBottom: theme.spacing(0.5),
+    },
+    tooltip: {
+      pointerEvents: 'auto !important' as any, // TS doesn't recognize !important as PointerEvent type
+      color: 'red',
+    },
+    button: {
+      marginTop: theme.spacing(1),
+    },
+  });
 
 interface FilterPanelProps extends WithStyles<typeof styles> {
   canLink: boolean;
@@ -106,7 +107,7 @@ type FilterType = {
   exclude: any;
   value: any;
   type: any;
-}
+};
 
 type FilterMap = any;
 
@@ -130,25 +131,24 @@ function FilterPanel({
   const [openFilter, setOpenFilter] = useState('');
 
   useEffect(() => {
-    setFilterMap(filterData)
-
+    setFilterMap(filterData);
   }, [filterMap, filterData]);
 
-  const handleChange = (column: string, filter: FilterType, table: string) => {
+  const handleChange = (column: string, filter: FilterType, tableName: string) => {
     const clonedMap: FilterMap = _.cloneDeep(filterMap);
     if (filter.value == null || filter.value.length === 0) {
-      delete clonedMap[table][column];
-      if (_.isEmpty(clonedMap[table])) {
-        delete clonedMap[table];
+      delete clonedMap[tableName][column];
+      if (_.isEmpty(clonedMap[tableName])) {
+        delete clonedMap[tableName];
       }
-    } else if (_.isPlainObject(clonedMap[table])) {
-      clonedMap[table][column] = {
+    } else if (_.isPlainObject(clonedMap[tableName])) {
+      clonedMap[tableName][column] = {
         exclude: filter.exclude,
         value: filter.value,
         type: filter.type,
       };
     } else {
-      clonedMap[table] = {
+      clonedMap[tableName] = {
         [column]: {
           exclude: filter.exclude,
           value: filter.value,
@@ -157,7 +157,7 @@ function FilterPanel({
       };
     }
     setFilterMap(clonedMap);
-    dispatch(applyFilters(clonedMap, tableName, dataset));
+    dispatch(applyFilters(filterMap, tableName, dataset));
   };
 
   const onPaste = (event: any) => {
@@ -181,7 +181,7 @@ function FilterPanel({
   };
 
   const handleSearchFilter = (_event: any, value: any) => {
-    setSearchStrings(value)
+    setSearchStrings(value);
     setSearchInput('');
   };
 
@@ -207,109 +207,105 @@ function FilterPanel({
       name: column.name,
     }));
 
-    const billingErrorMessage =
-      "You cannot create a snapshot because you do not have access to the dataset's billing profile.";
+  const billingErrorMessage =
+    "You cannot create a snapshot because you do not have access to the dataset's billing profile.";
 
-    return (
-      <div className={classes.root}>
-        <div className={classes.rowOne}>
-          <Box className={!open ? classes.hide : ''}>
-            <Grid container={true} spacing={1}>
-              <Grid item xs={10} className={classes.sidebarTitle}>
-                <Typography variant="h6" display="block">
-                  Select Data
-                </Typography>
-              </Grid>
+  return (
+    <div className={classes.root}>
+      <div className={classes.rowOne}>
+        <Box className={!open ? classes.hide : ''}>
+          <Grid container={true} spacing={1}>
+            <Grid item xs={10} className={classes.sidebarTitle}>
+              <Typography variant="h6" display="block">
+                Select Data
+              </Typography>
             </Grid>
-          </Box>
-          <div className={clsx(classes.filterPanel, { [classes.hide]: !open })}>
-            <DataSidebarPanel selected={selected} data-cy="snapshotCard" />
-          </div>
-          <Autocomplete
-            className={classes.inputBase}
-            multiple
-            freeSolo
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                variant="outlined"
-                fullWidth
-                className={classes.input}
-                placeholder="Search filters"
-                onChange={handleSearchString}
-                onKeyDown={handleReturn}
-                onPaste={onPaste}
-                data-cy="enterEmailBox"
-              />
-            )}
-            onChange={handleSearchFilter}
-            value={searchStrings}
-            inputValue={searchInput}
-            options={[]}
-          />
-          {table &&
-            table.name &&
-            filteredColumns.map((c: any) => (
-              <div
-                className={clsx({ [classes.highlighted]: c.name === openFilter })}
-                data-cy="filterItem"
-                key={c.name}
-              >
-                <ListItem
-                  button
-                  className={classes.filterListItem}
-                  onClick={() => handleOpenFilter(c)}
-                >
-                  {c.name}
-                  {c.name === openFilter ? <ExpandLess /> : <ExpandMore />}
-                </ListItem>
-                <Collapse
-                  in={c.name === openFilter}
-                  timeout="auto"
-                  className={classes.panelContent}
-                >
-                  <DataViewSidebarItem
-                    column={c}
-                    dataset={dataset}
-                    filterData={filterData}
-                    filterStatement={filterStatement}
-                    joinStatement={joinStatement}
-                    handleChange={handleChange}
-                    handleFilters={handleFilters}
-                    tableName={table.name}
-                    token={token}
-                  />
-                </Collapse>
-              </div>
-            ))}
+          </Grid>
+        </Box>
+        <div className={clsx(classes.filterPanel, { [classes.hide]: !open })}>
+          <DataSidebarPanel selected={selected} data-cy="snapshotCard" />
         </div>
-        <div className={clsx(classes.rowTwo, classes.snapshotBtnCntnr)}>
-          <Divider />
-          <TerraTooltip title={canLink ? '' : billingErrorMessage}>
-            <span>
-              <Button
-                variant="contained"
-                color="primary"
-                disableElevation
-                disabled={_.isEmpty(dataset?.schema?.assets) || !canLink}
-                className={clsx(
-                  {
-                    [classes.hide]: !open,
-                    [classes.tooltip]: _.isEmpty(dataset?.schema?.assets),
-                  },
-                  classes.button,
-                )}
-                onClick={() => handleCreateSnapshot(true)}
-                data-cy="createSnapshot"
+        <Autocomplete
+          className={classes.inputBase}
+          multiple
+          freeSolo
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="outlined"
+              fullWidth
+              className={classes.input}
+              placeholder="Search filters"
+              onChange={handleSearchString}
+              onKeyDown={handleReturn}
+              onPaste={onPaste}
+              data-cy="enterEmailBox"
+            />
+          )}
+          onChange={handleSearchFilter}
+          value={searchStrings}
+          inputValue={searchInput}
+          options={[]}
+        />
+        {table &&
+          table.name &&
+          filteredColumns.map((c: any) => (
+            <div
+              className={clsx({ [classes.highlighted]: c.name === openFilter })}
+              data-cy="filterItem"
+              key={c.name}
+            >
+              <ListItem
+                button
+                className={classes.filterListItem}
+                onClick={() => handleOpenFilter(c)}
               >
-                Next
-              </Button>
-            </span>
-          </TerraTooltip>
-        </div>
+                {c.name}
+                {c.name === openFilter ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={c.name === openFilter} timeout="auto" className={classes.panelContent}>
+                <DataViewSidebarItem
+                  column={c}
+                  dataset={dataset}
+                  filterData={filterData}
+                  filterStatement={filterStatement}
+                  joinStatement={joinStatement}
+                  handleFilterChange={handleChange}
+                  // handleFilters={handleFilters}
+                  tableName={table.name}
+                  token={token}
+                />
+              </Collapse>
+            </div>
+          ))}
       </div>
-    );
-  }
+      <div className={clsx(classes.rowTwo, classes.snapshotBtnCntnr)}>
+        <Divider />
+        <TerraTooltip title={canLink ? '' : billingErrorMessage}>
+          <span>
+            <Button
+              variant="contained"
+              color="primary"
+              disableElevation
+              disabled={_.isEmpty(dataset?.schema?.assets) || !canLink}
+              className={clsx(
+                {
+                  [classes.hide]: !open,
+                  [classes.tooltip]: _.isEmpty(dataset?.schema?.assets),
+                },
+                classes.button,
+              )}
+              onClick={() => handleCreateSnapshot(true)}
+              data-cy="createSnapshot"
+            >
+              Next
+            </Button>
+          </span>
+        </TerraTooltip>
+      </div>
+    </div>
+  );
+}
 
 function mapStateToProps(state: TdrState) {
   return {
