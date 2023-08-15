6d6d897a677d5748ce357fc9a5a632ff0d67215a
Saman Ehsan <se8ea@virginia.edu>
2023-08-15 11:05:06 -0400

    Fix cypress e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,6 @@
 import { devServer } from '@cypress/vite-dev-server';
 import { defineConfig } from 'cypress';
+import vitePreprocessor from 'cypress-vite';
 import customViteConfig from './vite.config';
 
 export default defineConfig({
@@ -14,6 +15,13 @@ export default defineConfig({
       });
     },
     specPattern: 'src/**/*test.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/index.js',
+  },
+  e2e: {
+    setupNodeEvents(on) {
+      on('file:preprocessor', vitePreprocessor());
+    },
+    specPattern: 'cypress/integration/**/*.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/index.js',
   },
   projectId: 'e6ttjx',
