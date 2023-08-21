64e2ad6d711bd58fb75567aef5a3a86f154f768a
Saman Ehsan <sehsan@broadinstitute.org>
2023-08-21 15:04:47 -0400

    Fix e2e test wait-on local server
    Work around wait-on 404 error
    
    Originally from: https://github.com/vitejs/vite/issues/9520#issuecomment-1660050942

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,20 @@ const bigQueryApi = 'https://bigquery.googleapis.com';
 const googleSheetsApi = 'https://sheets.googleapis.com';
 const driveApi = 'https://www.googleapis.com';
 
+function fixAcceptHeader404() {
+    return {
+        name: 'fix-accept-header-404', // issue with vite dev server: https://github.com/vitejs/vite/issues/9520
+        configureServer(server) {
+            server.middlewares.use((req, _res, next) => {
+                if (req.headers.accept == 'application/json, text/plain, */*') {
+                    req.headers.accept = '*/*';
+                }
+                next();
+            });
+        },
+    };
+}
+
 dns.setDefaultResultOrder('verbatim');
 
 export default defineConfig({
@@ -29,6 +43,7 @@ export default defineConfig({
     svgr(),
     tsconfigPaths(),
     eslint(),
+    fixAcceptHeader404(),
   ],
   server: {
     port: 3000,
