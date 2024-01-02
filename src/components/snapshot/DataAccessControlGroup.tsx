c8abd6eb12ebd7e0cca5c424f03863677f2ab854
Shelby Holden <sholden@broadinstitute.org>
2024-01-02 14:48:06 -0500

    Display description for auth domain group; Conditionally display group

--- src/components/snapshot/DataAccessControlGroup.tsx
+++ src/components/snapshot/DataAccessControlGroup.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { Typography, Grid } from '@mui/material';
+import { ClassNameMap, withStyles } from '@mui/styles';
+import { Typography, Grid, CustomTheme } from '@mui/material';
 import { TdrState } from '../../reducers';
 import UserList from '../UserList';
 
+const styles = (theme: CustomTheme) => ({
+  terraLink: {
+    color: theme.palette.primary.main,
+    paddingBottom: theme.spacing(4),
+    paddingTop: theme.spacing(2),
+    textDecoration: 'none',
+  },
+});
+
 type DataAccessControlGroupProps = {
+  classes: ClassNameMap;
   authDomains: Array<string>;
+  terraUrl: string;
 };
 
-function DataAccessControlGroup(props: DataAccessControlGroupProps) {
-  const { authDomains } = props;
+function DataAccessControlGroup({ classes, authDomains, terraUrl }: DataAccessControlGroupProps) {
   return (
     <Grid container spacing={1} data-cy="data-access-control-container">
       <Typography variant="h6">Data Access Control Groups</Typography>
+      <Grid item xs={12} data-cy="data-access-control-description">
+        <Typography variant="body1">
+          Collaborators must be a member of all of these{' '}
+          <a href={`${terraUrl}/#groups`} target="_blank" rel="noopener noreferrer">
+            <span className={classes.terraLink}>groups</span>
+          </a>{' '}
+          to access this snapshot.
+        </Typography>
+      </Grid>
       <Grid item xs={12} data-cy="data-access-controls">
         <UserList canManageUsers={false} typeOfUsers="Data Access Controls" users={authDomains} />
       </Grid>
@@ -24,7 +44,8 @@ function DataAccessControlGroup(props: DataAccessControlGroupProps) {
 function mapStateToProps(state: TdrState) {
   return {
     authDomains: state.snapshots.snapshotAuthDomains,
+    terraUrl: state.configuration.configObject.terraUrl,
   };
 }
 
-export default connect(mapStateToProps)(DataAccessControlGroup);
+export default connect(mapStateToProps)(withStyles(styles)(DataAccessControlGroup));
