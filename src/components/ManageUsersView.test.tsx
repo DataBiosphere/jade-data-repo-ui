caa975d09cd5dbc5ca0e90df6936c5a6856904eb
Shelby Holden <sholden@broadinstitute.org>
2024-01-03 09:44:24 -0500

    Revert "debug failing unit test"
    This reverts commit c4850315088c8034b9a332f23d4f9a8ab3911502.

--- src/components/ManageUsersView.test.tsx
+++ src/components/ManageUsersView.test.tsx
@@ -9,30 +9,30 @@ import globalTheme from '../modules/theme';
 import ManageUsersView from './ManageUsersView';
 
 describe('ManageUsersView', () => {
-  // [true, false].forEach((canManageUsers) => {
-  //   it('Renders user list indepedent of whether you can manage users', () => {
-  //     const mockStore = createMockStore([]);
-  //     const store = mockStore({});
-  //     mount(
-  //       <Router history={history}>
-  //         <Provider store={store}>
-  //           <ThemeProvider theme={globalTheme}>
-  //             <ManageUsersView
-  //               classes={{}}
-  //               removeUser={canManageUsers ? () => <div /> : undefined}
-  //               users={['authdomain1', 'authdomain2', 'authdomain3']}
-  //             />
-  //           </ThemeProvider>
-  //         </Provider>
-  //       </Router>,
-  //     );
-  //     cy.get('[data-cy=chip-container]').within(() => {
-  //       cy.contains('authdomain1').should('exist');
-  //       cy.contains('authdomain2').should('exist');
-  //       cy.contains('authdomain3').should('exist');
-  //     });
-  //   });
-  // });
+  [true, false].forEach((canManageUsers) => {
+    it('Renders user list indepedent of whether you can manage users', () => {
+      const mockStore = createMockStore([]);
+      const store = mockStore({});
+      mount(
+        <Router history={history}>
+          <Provider store={store}>
+            <ThemeProvider theme={globalTheme}>
+              <ManageUsersView
+                classes={{}}
+                removeUser={canManageUsers ? () => <div /> : undefined}
+                users={['authdomain1', 'authdomain2', 'authdomain3']}
+              />
+            </ThemeProvider>
+          </Provider>
+        </Router>,
+      );
+      cy.get('[data-cy=chip-container]').within(() => {
+        cy.contains('authdomain1').should('exist');
+        cy.contains('authdomain2').should('exist');
+        cy.contains('authdomain3').should('exist');
+      });
+    });
+  });
   it('No container when there are no users', () => {
     const mockStore = createMockStore([]);
     const store = mockStore({});
