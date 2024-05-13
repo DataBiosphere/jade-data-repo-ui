c9cd71b3822126e66fde8079fba8a48e5a8bfde6
Sky Rubenstein <blrubenstein@wpi.edu>
2024-05-13 10:44:44 -0400

    Remove unused props and styles

--- src/components/SnapshotAccessRequestView.tsx
+++ src/components/SnapshotAccessRequestView.tsx
@@ -1,42 +1,21 @@
 import { TdrState } from 'reducers';
 import { connect } from 'react-redux';
-import { WithStyles, withStyles } from '@mui/styles';
-import { CustomTheme } from '@mui/material/styles';
 import { SnapshotAccessRequestResponse } from 'generated/tdr';
-import React, { Dispatch } from 'react';
-import { Action } from 'redux';
+import React from 'react';
 
-const styles = (theme: CustomTheme) => ({
-  wrapper: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginTop: '1em',
-  },
-  width: {
-    ...theme.mixins.containerWidth,
-  },
-  title: {
-    color: theme.palette.primary.main,
-    fontSize: 54,
-    lineHeight: '66px',
-    paddingBottom: theme.spacing(8),
-  },
-});
-interface IProps extends WithStyles<typeof styles> {
+interface IProps {
   snapshotAccessRequests: Array<SnapshotAccessRequestResponse>;
-  dispatch: Dispatch<Action>;
-  loading: boolean;
-  refreshCnt: number;
-  userEmail: string;
 }
 
-const SnapshotAccessRequestView = withStyles(styles)(({ snapshotAccessRequests }: IProps) => (
-  <ul>
-    {snapshotAccessRequests.map((accessRequest, index) => (
-      <li key={index}>{accessRequest.id}</li>
-    ))}
-  </ul>
-));
+function SnapshotAccessRequestView({ snapshotAccessRequests }: IProps) {
+  return (
+    <ul>
+      {snapshotAccessRequests.map((accessRequest, index) => (
+        <li key={index}>{accessRequest.id}</li>
+      ))}
+    </ul>
+  );
+}
 
 function mapStateToProps(state: TdrState) {
   return {
