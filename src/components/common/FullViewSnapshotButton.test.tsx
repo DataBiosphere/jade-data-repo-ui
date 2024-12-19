3af7d581d5c33e6f4eab8401251a1cb06fb98a7b
rjohanek <rjohanek@broadinstitute.org>
2024-12-19 16:13:33 -0500

    first pass at tests

---
+++ src/components/common/FullViewSnapshotButton.test.tsx
@@ -0,0 +1,99 @@
+import { mount } from 'cypress/react';
+import { Router } from 'react-router-dom';
+import { ThemeProvider } from '@mui/styles';
+import { Provider } from 'react-redux';
+import React from 'react';
+import createMockStore from 'redux-mock-store';
+import history from '../../modules/hist';
+import globalTheme from '../../modules/theme';
+import FullViewSnapshotButton from './FullViewSnapshotButton';
+
+const initialState = {
+  snapshots: {
+    snapshot: {
+      id: 'uuid',
+      name: 'Test Snapshot',
+    },
+  },
+  profiles: {
+    profiles: [],
+  },
+};
+
+describe('FullViewSnapshotButton component with permission', () => {
+  beforeEach(() => {
+    const mockStore = createMockStore([]);
+    const store = mockStore(initialState);
+    const dataset = { defaultProfileId: 'profile1' };
+    // Intercept the getBillingProfiles API call onMount
+    cy.intercept('GET', '/api/resources/v1/profiles?offset=0&limit=1000', {
+      statusCode: 200,
+      body: [
+        {
+          id: 'profile1',
+          name: 'Test Profile 1',
+        },
+      ],
+    }).as('getBillingProfiles');
+
+    mount(
+      <Router history={history}>
+        <Provider store={store}>
+          <ThemeProvider theme={globalTheme}>
+            <FullViewSnapshotButton dataset={dataset} />
+          </ThemeProvider>
+        </Provider>
+      </Router>,
+    );
+  });
+
+  it('Displays the button with correct text', () => {
+    cy.get('button').should('contain.text', 'Create Full-View Snapshot');
+  });
+
+  it('Button is clickable, calls createSnapshot, and does not have a tooltip', () => {
+    cy.get('button').click();
+    cy.intercept('POST', '/api/repository/v1/snapshots');
+    cy.get('button').trigger('mouseover');
+    cy.contains(
+      'You do not have access to the billing profile associated with this dataset.',
+    ).should('not.be.visible');
+  });
+});
+
+describe('FullViewSnapshotButton component without permission', () => {
+  beforeEach(() => {
+    const mockStore = createMockStore([]);
+    const store = mockStore(initialState);
+    const dataset = { defaultProfileId: 'profile1' };
+
+    // Intercept the getBillingProfiles API call onMount
+    cy.intercept('GET', '/api/resources/v1/profiles?offset=0&limit=1000', {
+      statusCode: 200,
+      body: [
+        {
+          id: 'profile2',
+          name: 'Test Profile 2',
+        },
+      ],
+    }).as('getBillingProfiles');
+
+    mount(
+      <Router history={history}>
+        <Provider store={store}>
+          <ThemeProvider theme={globalTheme}>
+            <FullViewSnapshotButton dataset={dataset} />
+          </ThemeProvider>
+        </Provider>
+      </Router>,
+    );
+  });
+
+  it('Button is disabled and has tooltip', () => {
+    cy.get('button').should('be.disabled');
+    cy.get('button').trigger('mouseover', { force: true });
+    cy.contains(
+      'You do not have access to the billing profile associated with this dataset.',
+    ).should('be.visible');
+  });
+});
