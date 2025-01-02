a0abdfea9ce2dcadbd4ffad94db494c0409a3c81
Shelby Holden <sholden@broadinstitute.org>
2025-01-02 15:07:49 -0500

    Revert "HomeView - Copilot upgrade"
    This reverts commit 15d263ab5950a5161ae4290d0d4c1b3e6ad465ec.

--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -2,9 +2,11 @@ import React, { Dispatch, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Action } from 'redux';
+
 import { Button } from '@mui/material';
 import { AddCircle, Refresh } from '@mui/icons-material';
-import { styled } from '@mui/system';
+import { createStyles, WithStyles, withStyles } from '@mui/styles';
+import { CustomTheme } from '@mui/material/styles';
 import { RouterLocation, RouterRootState } from 'connected-react-router';
 import { LocationState } from 'history';
 import {
@@ -22,75 +24,70 @@ import JobView from './JobView';
 import SearchTable from './table/SearchTable';
 import SnapshotAccessRequestView from './SnapshotAccessRequestView';
 
-const PageRoot = styled('div')(({ theme }) => ({
-  padding: '16px 24px',
-  display: 'flex',
-  flexDirection: 'column',
-  height: '100%',
-}));
+const styles = (theme: CustomTheme) =>
+  createStyles({
+    pageRoot: {
+      padding: '16px 24px',
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100%',
+    },
+    header: {
+      alignItems: 'center',
+      color: theme.typography.color,
+      display: 'flex',
+      fontWeight: 500,
+      fontSize: 16,
+      height: 21,
+      letterSpacing: 1,
+    },
+    jadeTableSpacer: {
+      paddingBottom: theme.spacing(12),
+    },
+    jadeLink: {
+      ...theme.mixins.jadeLink,
+      float: 'right',
+      fontSize: 16,
+      fontWeight: 500,
+      height: 20,
+      letterSpacing: 0.3,
+      paddingLeft: theme.spacing(4),
+      paddingTop: theme.spacing(4),
+    },
+    title: {
+      color: theme.palette.secondary.dark,
+      fontSize: '1.5rem',
+      fontWeight: 700,
+      flex: '1 1 0',
+      'padding-right': '2em',
+      display: 'flex',
+    },
+    titleText: {
+      width: '150px',
+    },
+    titleAndSearch: {
+      display: 'flex',
+      'margin-top': '1.25em',
+      'margin-bottom': '1.25em',
+    },
+    headerButton: {
+      padding: 10,
+      marginLeft: theme.spacing(2),
+      height: '45px',
+      textTransform: 'none',
+    },
+    buttonIcon: {
+      marginRight: 6,
+      fontSize: '1.5rem',
+    },
+  });
 
-const Header = styled('div')(({ theme }) => ({
-  alignItems: 'center',
-  color: theme.typography.color,
-  display: 'flex',
-  fontWeight: 500,
-  fontSize: 16,
-  height: 21,
-  letterSpacing: 1,
-}));
-
-const JadeTableSpacer = styled('div')(({ theme }) => ({
-  paddingBottom: theme.spacing(12),
-}));
-
-const JadeLink = styled(Link)(({ theme }) => ({
-  ...theme.mixins.jadeLink,
-  float: 'right',
-  fontSize: 16,
-  fontWeight: 500,
-  height: 20,
-  letterSpacing: 0.3,
-  paddingLeft: theme.spacing(4),
-  paddingTop: theme.spacing(4),
-}));
-
-const Title = styled('div')(({ theme }) => ({
-  color: theme.palette.secondary.dark,
-  fontSize: '1.5rem',
-  fontWeight: 700,
-  flex: '1 1 0',
-  paddingRight: '2em',
-  display: 'flex',
-}));
-
-const TitleText = styled('span')({
-  width: '150px',
-});
-
-const TitleAndSearch = styled('div')({
-  display: 'flex',
-  marginTop: '1.25em',
-  marginBottom: '1.25em',
-});
-
-const HeaderButton = styled(Button)(({ theme }) => ({
-  padding: 10,
-  marginLeft: theme.spacing(2),
-  height: '45px',
-  textTransform: 'none',
-}));
-
-const ButtonIcon = styled(AddCircle)({
-  marginRight: 6,
-  fontSize: '1.5rem',
-});
-
-interface IProps {
+interface IProps extends WithStyles<typeof styles> {
   dispatch: Dispatch<Action>;
   location: RouterLocation<LocationState>;
 }
 
-function HomeView({ dispatch, location }: IProps) {
+function HomeView({ classes, dispatch, location }: IProps) {
   const [searchString, setSearchString] = useState('');
   const prefixMatcher = /\/[^/]*/;
   const tabValue = prefixMatcher.exec(location.pathname)?.[0];
@@ -118,32 +115,39 @@ function HomeView({ dispatch, location }: IProps) {
     refresh = () => dispatch(refreshSnapshotAccessRequests());
   }
   const refreshButton = (
-    <HeaderButton
+    <Button
       aria-label="refresh page"
       size="medium"
+      className={classes.headerButton}
       onClick={refresh}
       variant="outlined"
       startIcon={<Refresh />}
     >
       Refresh
-    </HeaderButton>
+    </Button>
   );
   const pageHeader =
     tabValue === '/datasets' ? (
-      <Title>
-        <TitleText>{pageTitle}</TitleText>
+      <div className={classes.title}>
+        <span className={classes.titleText}>{pageTitle}</span>
         {refreshButton}
-        <JadeLink to="datasets/new" data-cy="create-dataset-link">
-          <HeaderButton color="primary" variant="outlined" disableElevation size="medium">
-            <ButtonIcon /> Create Dataset
-          </HeaderButton>
-        </JadeLink>
-      </Title>
+        <Link to="datasets/new" data-cy="create-dataset-link">
+          <Button
+            className={classes.headerButton}
+            color="primary"
+            variant="outlined"
+            disableElevation
+            size="medium"
+          >
+            <AddCircle className={classes.buttonIcon} /> Create Dataset
+          </Button>
+        </Link>
+      </div>
     ) : (
-      <Title>
-        <TitleText>{pageTitle}</TitleText>
+      <div className={classes.title}>
+        <span className={classes.titleText}>{pageTitle}</span>
         {refreshButton}
-      </Title>
+      </div>
     );
 
   useOnMount(() => {
@@ -151,8 +155,8 @@ function HomeView({ dispatch, location }: IProps) {
   });
 
   return (
-    <PageRoot>
-      <TitleAndSearch>
+    <div className={classes.pageRoot}>
+      <div className={classes.titleAndSearch}>
         {pageHeader}
         {searchable && (
           <SearchTable
@@ -161,9 +165,9 @@ function HomeView({ dispatch, location }: IProps) {
             clearSearchString={() => setSearchString('')}
           />
         )}
-      </TitleAndSearch>
+      </div>
       {tableValue}
-    </PageRoot>
+    </div>
   );
 }
 
@@ -173,4 +177,4 @@ function mapStateToProps(state: TdrState & RouterRootState) {
   };
 }
 
-export default connect(mapStateToProps)(HomeView);
+export default connect(mapStateToProps)(withStyles(styles)(HomeView));
