aa6caf960a57fce9305b3fbfedbe6af42684225b
Shelby Holden <sholden@broadinstitute.org>
2025-01-03 10:57:25 -0500

    JobView - prep for copilot - remove unused styles

--- src/components/JobView.tsx
+++ src/components/JobView.tsx
@@ -18,12 +18,6 @@ const styles = (theme: CustomTheme) => ({
   width: {
     ...theme.mixins.containerWidth,
   },
-  title: {
-    color: theme.palette.primary.main,
-    fontSize: '54px',
-    lineHeight: '66px',
-    paddingBottom: theme.spacing(8),
-  },
 });
 
 interface IProps extends WithStyles<typeof styles> {
