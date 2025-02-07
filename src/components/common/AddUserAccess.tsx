63afb2834298132a2936c6788f6cb06d7eb4b1ee
rushtong <grushton@broadinstitute.org>
2025-02-07 09:42:47 -0500

    feat: revert

--- src/components/common/AddUserAccess.tsx
+++ src/components/common/AddUserAccess.tsx
@@ -27,8 +27,8 @@ export interface AccessPermission {
 }
 
 interface AddUserAccessProps {
-  readonly permissions: AccessPermission[];
-  readonly onAdd: (policyName: string, usersToAdd: string[]) => void;
+  permissions: AccessPermission[];
+  onAdd: (policyName: string, usersToAdd: string[]) => void;
 }
 
 // The rest can use sx prop as they have ≤3 styles
