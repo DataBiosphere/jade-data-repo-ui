c3cc9abc7c752f4985b514ad6e9a881116d5fd16
Sky Rubenstein <blrubenstein@wpi.edu>
2025-02-13 09:53:53 -0500

    Use AuthDomain component

--- src/components/common/FullViewSnapshotModal.tsx
+++ src/components/common/FullViewSnapshotModal.tsx
@@ -25,6 +25,7 @@ import { Action } from 'redux';
 import { ManagedGroupMembershipEntry } from 'models/group';
 import { styled } from '@mui/system';
 import { useTheme } from '@mui/styles';
+import AuthDomain from 'components/snapshot/AuthDomain';
 
 const StyledLink = styled('span')(({ theme }: { theme: CustomTheme }) => ({
   ...theme.mixins.jadeLink,
@@ -139,38 +140,7 @@ function FullViewSnapshotModal(props: FullViewSnapshotModalProps) {
           value={selectedBillingProfile?.profileName || ''}
         />
         <Box sx={{ marginTop: '8px' }}>
-          <FormLabel
-            sx={{ fontWeight: 600, color: 'black' }}
-            htmlFor="select-authorization-domain-select"
-          >
-            Authorization Domain
-            <Box component="span" sx={{ fontWeight: 400, color: 'black' }}>
-              {' '}
-              - (optional)
-            </Box>
-          </FormLabel>
-        </Box>
-        <JadeDropdown
-          sx={{ height: '2.5rem' }}
-          disabled={userGroups ? userGroups.length <= 1 : true}
-          options={userGroups ? userGroups.map((group) => group.groupName) : []}
-          name="Select Authorization Domain"
-          onSelectedItem={(event) => setSelectedAuthDomain(event.target.value)}
-          value={selectedAuthDomain || ''}
-          includeNoneOption={true}
-        />
-        <Box>
-          Authorization Domains restrict data access to only specified individuals in a group and
-          are intended to fulfill requirements you may have for data governed by a compliance
-          standard, such as federal controlled-access data or HIPAA protected data. They follow all
-          snapshot copies and cannot be removed. For more details, see{' '}
-          <Link
-            href="https://support.terra.bio/hc/en-us/articles/360026775691-Overview-Managing-access-to-controlled-data-with-Authorization-Domains#h_01J94P49XS1NE5KE4B3NA3A413"
-            target="_blank"
-          >
-            <StyledLink theme={useTheme()}>When to use an Authorization Domain</StyledLink>
-          </Link>
-          .
+          <AuthDomain setParentAuthDomain={setSelectedAuthDomain} />
         </Box>
         <DialogActions sx={{ display: 'flex', justifyContent: 'flex-end' }}>
           <Button onClick={onDismiss} variant="outlined">
