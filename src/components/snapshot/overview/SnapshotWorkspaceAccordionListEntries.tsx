3986eec703e8f96b9554544e6f8de0445358d293
rushtong <grushton@broadinstitute.org>
2025-02-24 14:57:23 -0500

    feat: convert SnapshotWorkspaceAccordionListEntries component

--- src/components/snapshot/overview/SnapshotWorkspaceAccordionListEntries.tsx
+++ src/components/snapshot/overview/SnapshotWorkspaceAccordionListEntries.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
 import { Launch } from '@mui/icons-material';
-import { CustomTheme, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import { createStyles, WithStyles, withStyles } from '@mui/styles';
+import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { CustomTheme } from '@mui/material/styles';
 import { SnapshotWorkspaceEntry } from '../../../models/workspaceentry';
 
-const styles = (theme: CustomTheme) =>
-  createStyles({
-    jadeLink: {
-      ...theme.mixins.jadeLink,
-    },
-    jadeIconLink: {
-      ...theme.mixins.jadeLink,
-      height: '0.75em',
-      width: '0.75em',
-    },
-  });
-
-interface SnapshotWorkspaceEntriesListProps extends WithStyles<typeof styles> {
+interface SnapshotWorkspaceEntriesListProps {
   entries: SnapshotWorkspaceEntry[];
 }
 
-function SnapshotWorkspaceEntriesList(props: SnapshotWorkspaceEntriesListProps) {
-  const { entries, classes } = props;
+export default function SnapshotWorkspaceEntriesList(props: SnapshotWorkspaceEntriesListProps) {
+  const { entries } = props;
   return (
     <>
       {entries.map((entry) => {
@@ -36,10 +24,23 @@ function SnapshotWorkspaceEntriesList(props: SnapshotWorkspaceEntriesListProps) 
               target="_blank"
               href={entry.link}
             >
-              <ListItemButton dense className={classes.jadeLink}>
+              <ListItemButton
+                dense
+                // @ts-ignore
+                sx={(theme: CustomTheme) => ({
+                  ...theme.mixins.jadeLink,
+                })}
+              >
                 {entry.title}
-                <ListItemIcon className={classes.jadeLink}>
-                  <Launch className={classes.jadeIconLink} />
+                <ListItemIcon
+                  // @ts-ignore
+                  sx={(theme: CustomTheme) => ({
+                    ...theme.mixins.jadeLink,
+                    height: '0.75em',
+                    width: '0.75em',
+                  })}
+                >
+                  <Launch />
                 </ListItemIcon>
               </ListItemButton>
             </ListItem>
@@ -54,5 +55,3 @@ function SnapshotWorkspaceEntriesList(props: SnapshotWorkspaceEntriesListProps) 
     </>
   );
 }
-
-export default withStyles(styles)(SnapshotWorkspaceEntriesList);
