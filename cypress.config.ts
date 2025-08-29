0d970bbc5a41d0d3c6f0485f2b8008bea59692eb
Kevin Marete <kevinmmarete@gmail.com>
2025-08-28 20:25:28 -0400

    Remove preprocessor

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,5 @@
-import { devServer } from '@cypress/vite-dev-server';
 import { defineConfig } from 'cypress';
-import vitePreprocessor from 'cypress-vite';
+import { devServer } from '@cypress/vite-dev-server';
 import customViteConfig from './vite.config';
 
 export default defineConfig({
@@ -17,9 +16,6 @@ export default defineConfig({
   },
   e2e: {
     baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on) {
-      on('file:preprocessor', vitePreprocessor());
-    },
     specPattern: 'cypress/integration/**/*.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/index.js',
   },
