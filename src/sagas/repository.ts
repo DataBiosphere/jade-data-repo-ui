e43137b39e9b0692ea6c1cc89cbc8c108e11cc77
Rae Johanek <rjohanek@broadinstitute.org>
2025-10-24 10:33:13 -0400

    update query to be more specific

--- src/sagas/repository.ts
+++ src/sagas/repository.ts
@@ -949,19 +949,18 @@ export function* getDuosDatasets(): any {
   try {
     const duosUrl = yield select(getDuosUrl);
     const url = `${duosUrl}/api/dataset/search/index`;
-    const params = {
-      query: {
-        bool: {
-          must: [
-            {
-              match: {
-                _type: 'dataset',
-              },
-            },
-          ],
-        },
-      },
+    const params = 
+    {
+      "query":
+      {"bool":
+        {"must":
+          [{"match":{"_type":"dataset"}},
+           {"term":{"study.publicVisibility": true}},
+           {"term":{"dataLocation.keyword": "TDR Location"}}]
+        }
+      }
     };
+
     const response = yield call(authPost, url, params);
     yield put({
       type: ActionTypes.GET_DUOS_DATASETS_SUCCESS,
